61533b723e68d2d08eb48602624e2887dd2659b8
isMedia <isMedia@DESKTOP-NRT9P4K>
2020-11-28 23:58:54 +0900

    test
    Co-Authored-By: ledpear <25046947+Ledpear@users.noreply.github.com>

--- PROGRAMMERS/Level 1/문자열 내 p와 y의 개수/PROGRAMMERS/PROGRAMMERS.cpp
+++ PROGRAMMERS/Level 1/문자열 내 p와 y의 개수/PROGRAMMERS/PROGRAMMERS.cpp
@@ -15,14 +15,27 @@ int main()
 
 
 
+
 	int nY = 0;
+
+
+
+
+
+
 	for (int i = 0; i < s.size(); i++)
 	{
 		if (s[i] == 'p' || s[i] == 'P')
 			nP++;
 		if (s[i] == 'y' || s[i] == 'Y')
 			nY++;
+
+
+
 	}
+
+
+
 
     return nP == nY;
 }
