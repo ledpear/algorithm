bdb96c30ab23e90bce9d2f53b27017586653386d
ledpear <tjsrb75@gmail.com>
2020-11-30 23:47:10 +0900

    프린터 and 멀쩡한 사각형

--- PROGRAMMERS/Level 2/멀쩡한 사각형/PROGRAMMERS/PROGRAMMERS.cpp
+++ PROGRAMMERS/Level 2/멀쩡한 사각형/PROGRAMMERS/PROGRAMMERS.cpp
@@ -7,10 +7,53 @@
 
 using namespace std;
 
+void NumSort(long long &a, long long &b)
+{
+	if (a < b)
+	{
+		long long nTemp = a;
+		a = b;
+		b = nTemp;
+	}
+}
+
 int main()
 {
-	int nLast = -1;
+	int w, h;
+	long long answer = 1;
+	w = 8;
+	h = 12;
 
-    return 0;
+	long long nMaxCross;
+	long long nCross = w;
+	long long tempX = w;
+	long long tempY = h;
+	long long nW = w;
+	long long nH = h;
+	NumSort(tempX, tempY);
+
+	if (w == h)
+	{
+		nMaxCross = w;
+	}
+	else
+	{
+		while (true)
+		{
+			if ((nCross = tempX % tempY) == 0)
+			{
+				nMaxCross = tempY;
+				break;
+			}
+			else
+			{
+				tempX = tempY;
+				tempY = nCross;
+				nMaxCross = nCross;
+			}
+		}
+	}
+
+	return (nW * nH) - (nW + nH - nMaxCross);
 }
 
