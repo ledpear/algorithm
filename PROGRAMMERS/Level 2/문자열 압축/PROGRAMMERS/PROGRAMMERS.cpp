fd782c09cad1fe0660dd892e6c406f006302b643
ledpear <tjsrb75@gmail.com>
2020-12-03 23:51:05 +0900

    문자열 압축 1차 - 풀이를 잘못함

---
+++ PROGRAMMERS/Level 2/문자열 압축/PROGRAMMERS/PROGRAMMERS.cpp
@@ -0,0 +1,71 @@
+// PROGRAMMERS.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int nLast = -1;
+	string s;
+	s = "abcabcabcabcdededededede";
+	int answer = 0;
+
+	// 1. 요약할 사이즈를 정한다.
+	// 2. 요약할 사이즈 만큼 문자열을 처음부터 끝까지 비교하면서 압축을 진행
+	// 3. 요약할 수 있으면 요약한 문자를 만들어 string에 누적하고 요약한 다음 위치부터 검색
+
+
+	//int pos = 0;
+	string temp;
+	string Result = "";
+	answer = s.size() - 1;
+	for (int nSize = 2; nSize <= s.size() / 2; nSize++) //요약 크기
+	{
+		for (int nPos = 0; s.size() - nPos >= nSize; nPos++) //요약 시작할 문자열
+		{
+			temp = s.substr(nPos, nSize);
+			if (s.substr(nPos + nSize, nSize) == temp)
+			{
+				int nCount = 1;
+				while (true)
+				{
+					if (nPos + (nSize * (nCount + 1)) > s.size())
+					{
+						break;
+					}
+					else if (s.substr(nPos + (nSize * nCount), nSize) == temp)
+					{
+						nCount++;
+					}
+					else
+					{
+						break;
+					}
+				}
+				Result += to_string(nCount);
+				Result += s.substr(nPos, nSize);
+				nPos = nPos + (nCount * nSize) - 1;
+			}
+			else
+			{
+				if (nPos + nSize == s.size())
+				{
+					Result += s.substr(nPos, nSize);
+				}
+				else
+					Result += s[nPos];
+				continue;
+			}
+		}
+		if (answer > Result.size())	answer = Result.size();
+		Result = "";
+	}
+
+    return 0;
+}
+
