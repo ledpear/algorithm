4098bfef3012db462d2b7516a5f8793b914b7d40
ledpear <tjsrb75@gmail.com>
2020-12-14 18:29:07 +0900

    베스트앨범

---
+++ PROGRAMMERS/Level 3/베스트앨범/PROGRAMMERS/PROGRAMMERS.cpp
@@ -0,0 +1,29 @@
+// PROGRAMMERS.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int nLast = -1;
+	vector<string> genres;
+	vector<int> plays;
+	vector<int> answer;
+
+
+	map<string, vector<int>> Music;
+
+	for (int i = 0; i < plays.size(); i++)
+	{
+
+	}
+
+
+    return 0;
+}
+
