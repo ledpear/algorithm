e3f3555748feba23a636f1d425cc1552928e2dd7
ledpear <tjsrb75@gmail.com>
2020-12-14 17:57:27 +0900

    가장 큰 정사각형 찾기 - 2차 시도

--- PROGRAMMERS/Level 2/가장 큰 정사각형 찾기/PROGRAMMERS/PROGRAMMERS.cpp
+++ PROGRAMMERS/Level 2/가장 큰 정사각형 찾기/PROGRAMMERS/PROGRAMMERS.cpp
@@ -4,18 +4,33 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+struct vPoint {   // 구조체 정의
+	int X;
+	int Y;
+	int Size;
+};
+
+bool compare(vPoint a, vPoint b)
+{
+	return a.Size > b.Size;
+}
 
 int main()
 {
 	vector<vector<int>> board = { {0, 0, 1, 1},{1, 1, 1, 1}};
 	//vector<vector<int>> board = { {0, 1, 1, 1},{1, 1, 1, 1},{1, 1, 1, 1},{0, 0, 1, 0} };
 	
+	int answer;
 	int nHeight = board.size();
 	int nWidth = board[0].size();
 
 	vector<vector<int>> vPass(nHeight, vector<int>(nWidth, 0));
+
+	vector<vPoint> vPoint;
 
 	int nMax = -1;
 
@@ -23,12 +38,15 @@ int main()
 	{
 		for (int nX = 0; nX < nWidth; nX++)
 		{
-			if (board[nY][nX] == 1 && vPass[nY][nX] == 0)
+			if (board[nY][nX] == 1)
 			{
 				//해당 보드가 1이면 탐색시작
 				int nSize = 1;
 				
+				struct vPoint vTemp;
 				vPass[nY][nX] = true;
+				vTemp.X = nX;
+				vTemp.Y = nY;
 
 				while (true)
 				{
@@ -36,47 +54,15 @@ int main()
 					if (nY + nSize >= nHeight || nX + nSize >= nWidth)
 					{
 						--nSize;
+
 						break;
 					}
-					bool bResult = true;
 
 					int y = nY + nSize;
 					int x = nX + nSize;
-					//y방향 확인
-					for (int i = nY; i <= y; i++)
-					{
-						if (board[i][x] == 0)
-						{
-							bResult = false;
-							break;
-						}
-					}
-					//y에서 0이 안나왔으면 x방향 확인, 
-					if (bResult)
-					{
-						for (int i = nX; i < x; i++)
-						{
-							if (board[y][i] == 0)
-							{
-								bResult = false;
-								break;
-							}
-						}
-					}
+					bool bResult = true;
 
-					if (bResult)
-					{
-						for (int i = nY; i <= y; i++)
-						{
-							vPass[i][x] = true;
-						}
-						for (int i = nX; i < x; i++)
-						{
-							vPass[y][i] = true;
-						}
-					}
-
-					if (bResult)	// 모두 통과 했으면 사이즈를 키워서 확인
+					if (board[nY][x] == 1 && board[y][nX] == 1)
 					{
 						++nSize;
 					}
@@ -84,20 +70,43 @@ int main()
 					{
 						--nSize;
 						break;
-					}
+					}					
 				}
 
-				if (nMax < nSize) nMax = nSize;
+				vTemp.Size = nSize;
+				vPoint.push_back(vTemp);
 			}
-
-			
 		}
 	}
-	int answer;
-	if (nMax == -1)
-		answer = 0;
-	else
-		answer = ++nMax * nMax;
+
+	sort(vPoint.begin(), vPoint.end(), compare);
+
+	for (int i = 0; i < vPoint.size(); i++)
+	{
+		int nX = vPoint[i].X;
+		int nY = vPoint[i].Y;
+		int nSize = vPoint[i].Size;
+
+		bool bResult = true;
+
+		for (int j = nY; j <= nY + nSize; j++)
+		{
+			for (int i = nX; i <= nX + nSize; i++)
+			{
+				if (board[j][i] == 0)
+				{
+					bResult = false;
+					break;
+				}
+			}
+		}
+
+		if (bResult)
+		{
+			answer = ++nSize * nSize;
+			break;
+		}
+	}
 
     return 0;
 }
