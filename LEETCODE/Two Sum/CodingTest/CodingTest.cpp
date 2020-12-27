7085e7ffd6e1923fd24b9c42d2308d5f0756a004
ledpear <tjsrb75@gmail.com>
2020-12-27 18:19:53 +0900

    Two Sum 추가 및 Sample 수정

---
+++ LEETCODE/Two Sum/CodingTest/CodingTest.cpp
@@ -0,0 +1,17 @@
+// CodingTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int target;
+	vector<int> nums;
+
+    return 0;
+}
+
--- LEETCODE/_Sample/CodingTest/CodingTest.cpp
+++ LEETCODE/_Sample/CodingTest/CodingTest.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     return 0;
