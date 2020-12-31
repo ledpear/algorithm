152e4b8e6ffc24a5d4d7bc8c23dcc1c0cfca942b
ledpear <tjsrb75@gmail.com>
2020-12-31 22:06:58 +0900

    Longest Palindromic Substring 시간초과

--- LEETCODE/5. Longest Palindromic Substring/CodingTest/CodingTest.cpp
+++ LEETCODE/5. Longest Palindromic Substring/CodingTest/CodingTest.cpp
@@ -9,58 +9,116 @@ using namespace std;
 
 int main()
 {
-	string s;
+	string s = "abbcccbbbcaaccbababcbcabca";
 	
-
+	string strResult;
+	string MaxResult = "";
 	int nMax = 0;
 
 	for (int i = 0; i < s.size(); i++)
 	{
 		for (int j = 1; j <= s.size() - i; j++)
 		{
+			if (i + j * 2 - 1> s.size())
+			{
+				if (MaxResult.compare("") == 0)	MaxResult += s[i];
+				break;
+			}
+
 			string temp = s.substr(i, j);
-			
-			int strPos;
-			bool bResult = true;
+			string strFirst, strSecond;
+			strFirst = temp;
+			strSecond = temp;
+			strResult = temp;
+
+			int strFirstPos, strSecondPos;
+			bool bResult = false;
+			bool bOverlap = false;
+
+			int ComparePos = i + j;
+
+			bool FirstCompare = false;
+			bool SecondCompare = false;
 
 			// 검사할 문자열의 사이즈가 1이 아닐 때
 			if (temp.size() != 1)
 			{
-				if (s[i + j + 1] == temp[temp.size() - 1])//반복하는지 검사할 다음 문자가 끝이나 끝 전이랑 같은지 확인
+				if (i + j * 2 <= s.size() && s[ComparePos] == temp[temp.size() - 1])//반복하는지 검사할 다음 문자가 끝이나 끝 전이랑 같은지 확인
 				{
-					strPos = temp.size() - 1;
+					strFirstPos = temp.size() - 1;
+					bOverlap = true;
+					strFirst += s[ComparePos];
+					FirstCompare = true;
 				}
-				else if (s[i + j + 1] == temp[temp.size() - 2])
+				if (s[ComparePos] == temp[temp.size() - 2])
 				{
-					strPos = temp.size() - 2;
+					strSecondPos = temp.size() - 2;
+					strSecond += s[ComparePos];
+					SecondCompare = true;
 				}
-				else //둘다 같지 않으면 반복하지 않는 것이기 때문에 Fail처리
-				{
-					strPos = -1;
-				}
+
+				bResult = FirstCompare || SecondCompare;
 
 				// 반복되는 문자인지 판단
-				if (strPos != -1)
+				if (bResult)
 				{
-					for (--strPos; strPos >= 0; strPos--)
+					bool FirstSuccess = FirstCompare;
+					bool SecondSuccess = SecondCompare;
+					
+					if (FirstCompare)
 					{
-						int s_pos = i + j + (temp.size() - strPos - 1);
-
-						bResult = (s[s_pos] == temp[strPos]);
-						if (!bResult) false;
+						int Pos = ComparePos;
+						for (--strFirstPos; strFirstPos >= 0; strFirstPos--)
+						{
+							if (FirstSuccess = s[++Pos] == temp[strFirstPos])
+							{
+								strFirst += s[Pos];
+							}
+							if (!FirstSuccess) break;
+						}
 					}
+					if (SecondCompare)
+					{
+						int Pos = ComparePos;
+						for (--strSecondPos; strSecondPos >= 0; strSecondPos--)
+						{
+							if (SecondSuccess = s[++Pos] == temp[strSecondPos])
+							{
+								strSecond += s[Pos];
+							}
+							if (!SecondSuccess) break;;
+						}
+					}
+					if (FirstSuccess)
+					{
+						strResult = strFirst;
+					}
+					else if (SecondSuccess)
+					{
+						strResult = strSecond;
+					}
+
+					bResult = FirstSuccess || SecondSuccess;
 				}
 			}
 			else //사이즈가 1일 때
 			{
-
+				bResult = true;
+				if (s[i] == s[i + 1])//반복하는지 검사할 다음 문자가 끝이나 끝 전이랑 같은지 확인
+				{
+					strResult += s[i + 1];
+					bOverlap = true;
+				}
 			}
 			
 			// 결과 적용
 			if (bResult)
 			{
-				if (nMax < temp.size())
-					nMax = temp.size();
+				if (nMax < strResult.size())
+				{
+					nMax = strResult.size();
+					MaxResult = strResult;
+				}					
 			}
 		}
 	}
