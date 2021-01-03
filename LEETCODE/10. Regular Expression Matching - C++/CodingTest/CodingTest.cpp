240b5bdeeb49320db69d47ae0c0d27953e3073fe
ledpear <tjsrb75@gmail.com>
2021-01-03 23:04:58 +0900

    Regular Expression Matching 내 방식대로 풀이 통과

--- LEETCODE/10. Regular Expression Matching - C++/CodingTest/CodingTest.cpp
+++ LEETCODE/10. Regular Expression Matching - C++/CodingTest/CodingTest.cpp
@@ -9,31 +9,56 @@ using namespace std;
 
 class Solution {
 public:
-	bool isMatch(string s, string p) {
+	bool isMatch(string s, string p, bool bLoop = false) {
 		int s_size = s.size();
 		int p_size = p.size();
+
+		int s_pos = s_size - 1;
+		int p_pos = p_size - 1;
 
 		bool bResult;
 
 		if (p_size == 0)
 			return s_size == 0;
-		bResult = (s_size != 0 && (p[0] == s[0] || p[0] == '.'));
 
-		if (p_size >= 2 && p[1] == '*')
+		if (s_size == 0)
 		{
-			return (isMatch(s, p.substr(2)) || (bResult && isMatch(s.substr(1), p)));
+			int count = 0;
+			for (int i = 0; i < p_size; i++)
+			{
+				if (p[i] == '*')
+					count++;
+			}
+			return count * 2 == p_size;
+		}
+
+		if (bResult = (s_size != 0 && (p[p_pos] == s[s_pos] || p[p_pos] == '.')))
+		{
+			if (bLoop)
+			{
+				return (isMatch(s, p.substr(0, p_pos - 1)) || (bResult && isMatch(s.substr(0, s_pos), p, true)));
+			}
+			else
+			{ 
+				return bResult && isMatch(s.substr(0, s_pos), p.substr(0, p_pos));
+			}
+		}
+		else if (p_size >= 2 && p[p_pos] == '*')
+		{
+			return (((p[p_pos - 1] == s[s_pos] || p[p_pos-1] == '.') && isMatch(s.substr(0, s_pos), p, true))
+				|| isMatch(s, p.substr(0, p_pos - 1)));
 		}
 		else
 		{
-			return bResult && isMatch(s.substr(1), p.substr(1));
+			return bResult && isMatch(s.substr(0, s_pos), p.substr(0, p_pos));//false
 		}
 	}
 };
 
 int main()
 {
-	string s = "aaaaaa";
-	string p = "b***";
+	string s = "aab";
+	string p = "b.*";
 
 	Solution sol;
 	bool bResult = sol.isMatch(s, p);
