4f9c04d0218f01a516e8ca71507c0d71ac7cc145
ledpear <tjsrb75@gmail.com>
2021-01-08 23:15:34 +0900

    14. Longest Common Prefix - Python 속도 단축

--- LEETCODE/14. Longest Common Prefix - Python/CodingTest.py
+++ LEETCODE/14. Longest Common Prefix - Python/CodingTest.py
@@ -1,23 +1,45 @@
 class Solution:
+    def CompareWord(str1, str2) -> str:
+
+        if len(str1) < len(str2) :
+            compare1 = str1
+            compare2 = str2
+        else:
+            compare1 = str2
+            compare2 = str1
+
+        strTest = ""
+        for i in range(len(compare1)) :
+            if compare1[i] == compare2[i] :
+                strTest += compare1[i]
+            else:
+                break
+
+        return strTest
+
+    def DivisionList(strs) -> str:
+        size = len(strs)
+        halfSize = size//2
+        strResult = ""
+        if size > 2 :
+            strs1 = list(strs[:halfSize])
+            strs2 = list(strs[halfSize:])
+
+            strResult = Solution.CompareWord(Solution.DivisionList(strs1), Solution.DivisionList(strs2))
+
+            return strResult
+        elif size == 2 :
+            strResult = Solution.CompareWord(strs[0],strs[1])
+
+            return strResult
+        elif size == 1 :
+            return strs[0]
+        else:
+            return ""
+
     def longestCommonPrefix(self, strs) -> str:
-        strs.sort(key=lambda x : len(x))
-        if len(strs) < 1 : return ""
 
-        tempStr = strs[0]
-        ResultStr = ""
-        TestStr = ""
-        for i in range(0, len(tempStr)) :
-            TestStr += tempStr[i]
-            bResult = True
-            for j in range(1, len(strs)) :
-                bResult &= strs[j].find(TestStr) == 0
-                if bResult == False : break
-
-            if bResult == True :
-                if len(ResultStr) < len(TestStr) : ResultStr = TestStr
-                else : break
-
-        return ResultStr
+        return Solution.DivisionList(strs)
 
 
 
