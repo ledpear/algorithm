cb1a1b0d713bb4e0313b6f2fae04724b84dc065b
ledpear <tjsrb75@gmail.com>
2021-01-13 10:32:52 +0900

    N과 M(1) 통과

--- BAEKJOON/10000~/10814_나이순 정렬/Source.cpp
+++ BAEKJOON/10000~/10814_나이순 정렬/Source.cpp
@@ -12,23 +12,23 @@ int main()
 	cin.tie(NULL);
 	int input;
 	cin >> input;
-	vector<vector<string>> data(201);
+	vector<vector<string>> data(201);	// 입력 나이가 1에서 200 사이기 때문에 201로 초기화
 
 	int nTemp;
 	string strTemp;
 	for (int i = 0; i < input; i++)
 	{
 		cin >> nTemp >> strTemp;
-		data[nTemp].push_back(strTemp);
+		data[nTemp].push_back(strTemp);	// 입력 나이에 해당하는 배열에 string 추가
 	}
 
 	for (int i = 1; i <= 200; i++)
 	{
-		if ((nTemp = data[i].size()) != 0)
+		if ((nTemp = data[i].size()) != 0)	//추가된 string이 있으면
 		{
 			for (int j = 0; j < nTemp; j++)
 			{
-				printf("%d %s\n", i, data[i][j].c_str());
+				printf("%d %s\n", i, data[i][j].c_str());	//순서대로 출력
 			}
 		}
 	}
---
+++ BAEKJOON/15000~/15649_N과 M(1)/Source.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void BackTracking(string strResult, int MaxNum, int size, vector<bool> vUsed)
+{
+	if (strResult.size() == size)
+	{
+		int i;
+		for (i = 0; i < size - 1; i++)
+		{
+			cout << strResult[i] << " ";
+		}
+		cout << strResult[i] << "\n";
+	}
+	else
+	{
+		for (int i = 1; i <= MaxNum; i++)
+		{
+			if(vUsed[i]) continue;
+			else
+			{
+				string strTemp = strResult;
+				vector<bool> vTemp = vUsed;
+
+				strTemp += to_string(i);
+				vTemp[i] = true;
+
+				BackTracking(strTemp, MaxNum, size, vTemp);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	int size = M;
+	int MaxNum = N;
+
+	string strResult = "";
+	vector<bool> vUsed(N + 1, 0);
+	
+	BackTracking(strResult, MaxNum, size, vUsed);
+
+	system("pause");
+
+	return 0;
+}
