159db5871723427de92b6540b89a15e4047e9014
ledpear <tjsrb75@gmail.com>
2021-01-14 10:55:53 +0900

    최단경로 - 일반적인 다익스트라 알고리즘

--- BAEKJOON/1000~/1753_최단경로/Source.cpp
+++ BAEKJOON/1000~/1753_최단경로/Source.cpp
@@ -4,16 +4,28 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
+#include <utility>
 
 using namespace std;
 
-#define INF		INT_MAX
+#define INF		987654321
 
 int FindMin(vector<int> vWeight, vector<bool> vVisit)
 {
 	int nMin = INF;
-
-
+	int nNodeNum = 0;
+	for (int i = 1; i < vWeight.size(); i++)
+	{
+		if (!vVisit[i])
+		{
+			if (nMin > vWeight[i])
+			{
+				nMin = vWeight[i];
+				nNodeNum = i;
+			}
+		}
+	}
+	return nNodeNum;
 }
 
 int main()
@@ -27,6 +39,43 @@ int main()
 	cin >> StartNode;
 
 	vWeight[StartNode] = 0;
+
+	vector<vector<pair<int, int>>> vEdge(V + 1);
+
+	int inputEdge;
+	for (int i = 0; i < E; i++)
+	{
+		cin >> inputEdge;
+		pair<int, int> pTemp;
+		cin >> pTemp.first >> pTemp.second;
+		vEdge[inputEdge].push_back(pTemp);
+	}
+
+	// 시작점으로 사용하지 않은 노드 중 최소 노드를 찾는다.
+	// 해당 노드가 갈 수 있는 간선을 찾아 가중치와 값을 더 해서 현제값과 비교해 더 작은 값을 넣는다
+
+	int FindNode = 0;
+	for (int i = 1; i < V; i++)
+	{
+		FindNode = FindMin(vWeight, vVisit);
+		vVisit[FindNode] = true;
+
+		for (int j = 0; j < vEdge[FindNode].size(); j++)
+		{
+			if (vWeight[vEdge[FindNode][j].first] > (vWeight[FindNode] + vEdge[FindNode][j].second))
+			{
+				vWeight[vEdge[FindNode][j].first] = (vWeight[FindNode] + vEdge[FindNode][j].second);
+			}
+		}
+	}
+
+	for (int i = 1; i < V; i++)
+	{
+		if (vWeight[i] == INF)
+			cout << "INF\n";
+		else
+			cout << vWeight[i] << "\n";
+	}
 
 	return 0;
 }
