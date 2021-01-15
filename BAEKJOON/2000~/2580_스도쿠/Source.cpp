1546ebe705622b60bee56af4cfac84365858d531
ledpear <tjsrb75@gmail.com>
2021-01-15 23:50:42 +0900

    스도쿠 중간저장

--- BAEKJOON/2000~/2580_스도쿠/Source.cpp
+++ BAEKJOON/2000~/2580_스도쿠/Source.cpp
@@ -1,50 +1,54 @@
 #include <vector>
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
 vector<vector<int>> gMap;
+bool bResult;
 
 void BackTracking(vector<vector<int>> vMap, vector<vector<int>> vRow, vector<vector<int>> vColumn, vector<vector<int>> vArea,
-	bool &bFind)
+	vector<pair<int, int>> vFind, int nCnt)
 {
-	bool bResult = true;
-	if (!bFind)
+	if (vFind.size() != nCnt)
 	{
-		for (int i = 1; i < 10; i++)
+		pair<int, int> pTemp = vFind[nCnt];
+
+		int nX = pTemp.second;
+		int nY = pTemp.first;
+
+		for (int a = 1; a <= 9; a++)
 		{
-			for (int j = 1; j < 10; j++)
+			if (vRow[nY][a] == false && vColumn[nX][a] == false && vArea[(((nY - 1) / 3) * 3) + (((nX - 1) / 3) % 3) + 1][a] == false)
 			{
-				if (vMap[i][j] == 0)
-				{
-					bResult = false;
-					for (int a = 1; a < 10; a++)
-					{
-						if (vRow[i][a] == 0 && vColumn[j][a] == 0 &&
-							vArea[(((i - 1) / 3) * 3) + (((j - 1) / 3) % 3) + 1][a] == 0)
-						{
-							vector<vector<int>> vTampMap = vMap;
-							vector<vector<int>> vTampRow = vRow;
-							vector<vector<int>> vTampColumn = vColumn;
-							vector<vector<int>> vTampArea = vArea;
-
-							vTampMap[i][j] = a;
-							vTampRow[i][a] = true;
-							vTampColumn[j][a] = true;
-							vTampArea[(((i - 1) / 3) * 3) + (((j - 1) / 3) % 3) + 1][a] = true;
-
-							BackTracking(vTampMap, vTampRow, vTampColumn, vTampArea, bFind);
-						}
-					}
-				}
+				vMap[nY][nX] = a;
+				vRow[nY][a] = true;
+				vColumn[nX][a] = true;
+				vArea[(((nY - 1) / 3) * 3) + (((nX - 1) / 3) % 3) + 1][a] = true;
+				BackTracking(vMap, vRow, vColumn, vArea, vFind, nCnt + 1);
+				vMap[nY][nX] = 0;
+				vRow[nY][a] = false;
+				vColumn[nX][a] = false;
+				vArea[(((nY - 1) / 3) * 3) + (((nX - 1) / 3) % 3) + 1][a] = false;
 			}
 		}
+	}
+	else
+	{
+		gMap = vMap;
 
-		if (bResult)
+		for (int i = 1; i < 10; i++)
 		{
-			bFind = true;
-			gMap = vMap;
+			int j;
+			for (j = 1; j < 9; j++)
+			{
+				cout << gMap[i][j] << " ";
+			}
+			cout << gMap[i][j] << "\n";
 		}
+
+		system("pause");
+		exit(0);
 	}
 }
 
@@ -55,42 +59,32 @@ int main()
 	vector<vector<int>> vColumn = vMap;
 	vector<vector<int>> vArea = vMap;
 
+	vector<pair<int, int>> vFind;
+
 	for (int i = 1; i < 10; i++)
 	{
 		cin >> vMap[i][1] >> vMap[i][2] >> vMap[i][3] >> vMap[i][4] >> vMap[i][5] >>
 			vMap[i][6] >> vMap[i][7] >> vMap[i][8] >> vMap[i][9];
-	}
 
-	for (int i = 1; i < 10; i++)
-	{
 		for (int j = 1; j < 10; j++)
 		{
 			if (vMap[i][j] != 0)
 			{
 				vRow[i][vMap[i][j]] = true;
 				vColumn[j][vMap[i][j]] = true;
-				vArea[(((i- 1) / 3) * 3) + (((j-1)/3)% 3) + 1][vMap[i][j]] = true;
+				vArea[(((i - 1) / 3) * 3) + (((j - 1) / 3) % 3) + 1][vMap[i][j]] = true;
 			}
-		}
-	}
-	bool bFind = false;
-	gMap = vMap;
-	BackTracking(vMap, vRow, vColumn, vArea, bFind);
-
-	if (gMap != vMap)
-	{
-		for (int i = 1; i < 10; i++)
-		{
-			int j;
-			for (j = 1; j < 9; j++)
+			else
 			{
-				cout << gMap[i][j] << " ";
+				vFind.push_back(make_pair(i, j));
 			}
-			cout << gMap[i][j] << "\n";
 		}
 	}
 
-	system("pause");
+	gMap = vMap;
+	bResult = false;
+
+	BackTracking(vMap, vRow, vColumn, vArea, vFind, 0);
 
 	return 0;
 }
