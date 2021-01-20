d7590ea8e6517b8e31ee5e4ad5ee31f8cb77432f
ledpear <tjsrb75@gmail.com>
2021-01-20 19:44:53 +0900

    계단 오르기 통과

--- BAEKJOON/2000~/2579_계단 오르기/Source.cpp
+++ BAEKJOON/2000~/2579_계단 오르기/Source.cpp
@@ -15,7 +15,26 @@ int main()
 	for (int i = 0; i < input; i++)
 		cin >> vMap[i];
 
+	vector<int> vDP(input);
 
+	if (input == 1)
+		cout << vMap[0] << '\n';
+	else if(input == 2)
+		cout << vMap[1] + vMap[0] << '\n';
+	else if(input == 3)
+		cout << max(vMap[0] + vMap[2], vMap[1] + vMap[2]) << '\n';
+	else
+	{
+		vDP[0] = vMap[0];
+		vDP[1] = vMap[1] + vMap[0];
+		vDP[2] = max(vMap[0] + vMap[2], vMap[1] + vMap[2]);
 
+		for (int i = 3; i < input; i++)
+		{
+			vDP[i] = max(vMap[i] + vDP[i - 2], vMap[i] + vMap[i - 1] + vDP[i - 3]);
+		}
+
+		cout << vDP[input - 1] << '\n';
+	}
 	return 0;
 }
