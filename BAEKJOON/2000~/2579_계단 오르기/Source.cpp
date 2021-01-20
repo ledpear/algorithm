e84c6cf7ed1c43404c3ff1fc654414e3d777a8a9
ledpear <tjsrb75@gmail.com>
2021-01-20 18:11:18 +0900

    계단 오르기 진행중

---
+++ BAEKJOON/2000~/2579_계단 오르기/Source.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	cin >> input;
+	vector<int> vMap(input);
+	for (int i = 0; i < input; i++)
+		cin >> vMap[i];
+
+
+
+	return 0;
+}
