21fa50fd9f224f99a0aed4b73d5289305adc030f
ledpear <tjsrb75@gmail.com>
2021-01-23 21:23:17 +0900

    14003 가장 긴 증가하는 부분 수열 5 통과

--- BAEKJOON/14000~/14003_가장 긴 증가하는 부분 수열 5/Source.cpp
+++ BAEKJOON/14000~/14003_가장 긴 증가하는 부분 수열 5/Source.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -12,18 +13,43 @@ int main()
 	int nSize;
 	cin >> nSize;
 	vector<int> nDp;
-	nDp.push_back(0);
 	int number;
+	vector<int> vScore(nSize);
+	vector<int> vArr(nSize);
 	for (int i = 0; i < nSize; i++)
 	{
 		cin >> number;
-		if (nDp.back() < number)
+		vArr[i] = number;
+		if (nDp.size() == 0 || nDp.back() < number)
+		{
 			nDp.push_back(number);
+			vScore[i] = nDp.size();
+		}
 		else
-			*lower_bound(nDp.begin(), nDp.end(), number) = number;
+		{
+			int nIndex = lower_bound(nDp.begin(), nDp.end(), number) - nDp.begin();
+			nDp[nIndex] = number;
+			vScore[i] = nIndex + 1;
+		}
 	}
 
-	cout << nDp.size() - 1 << '\n';
+	cout << nDp.size() << '\n';
+	int nCount = nDp.size();
+	stack<int> sOutput;
+	for (int i = nSize - 1; i >= 0; i--)
+	{
+		if (nCount == vScore[i])
+		{
+			sOutput.push(vArr[i]);
+			nCount--;
+		}
+	}
+	while (!sOutput.empty())
+	{
+		cout << sOutput.top() << ' ';
+		sOutput.pop();
+	}
+	cout << '\n';
 
 	return 0;
 }
