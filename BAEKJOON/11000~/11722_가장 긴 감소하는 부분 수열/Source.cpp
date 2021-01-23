0688532931301a239c05257e073e2411080e7bfa
ledpear <tjsrb75@gmail.com>
2021-01-23 16:26:27 +0900

    11722 가장 긴 감소하는 부분 수열 통과

---
+++ BAEKJOON/11000~/11722_가장 긴 감소하는 부분 수열/Source.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int nSize;
+	cin >> nSize;
+	vector<int> vArr(nSize);
+	vector<int> vDp(nSize);
+	for (int i = 0; i < nSize; i++)
+		cin >> vArr[i];
+	int nMax = 0;
+	for (int i = 0; i < nSize; i++)
+	{
+		vDp[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (vArr[j] > vArr[i])
+				vDp[i] = max(vDp[i], vDp[j] + 1);
+		}
+		if (nMax < vDp[i])
+			nMax = vDp[i];
+	}
+
+	cout << nMax << '\n';
+
+	system("pause");
+
+	return 0;
+}
