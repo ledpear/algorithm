260fa29aa895e8e543b78c947da213a25dd8111b
ledpear <tjsrb75@gmail.com>
2021-01-24 13:44:07 +0900

    1541 잃어버린 괄호 통과

--- BAEKJOON/1000~/1541_잃어버린 괄호/Source.cpp
+++ BAEKJOON/1000~/1541_잃어버린 괄호/Source.cpp
@@ -6,93 +6,37 @@
 #include <string>
 
 using namespace std;
-#define INF -987654321
 
 int main()
 {
 	string input;
 	cin >> input;
 
-	vector<int> vNumber;
-	vector<char> vOper;
-	string strTemp1 = "";
-	string strTemp2 = "";
+	string strTemp = "";
+	bool bMinus = false;
+	int nSum = 0;
 
-	strTemp1 += input[0];
-	
-	string strMinus = "";
-	bool bPlus = false;
-	for (int i = 1; i < input.size(); i++)
+	for (int i = 0; i < input.size(); i++)
 	{
 		if (input[i] == '+' || input[i] == '-')
 		{
-			if (bPlus)
-			{
-				int nTemp = stoi(strTemp1) + stoi(strTemp2);
-				strTemp2 = to_string(nTemp);
-				bPlus = false;
-			}
+			if (bMinus)
+				nSum -= stoi(strTemp);
+			else
+				nSum += stoi(strTemp);
+
+			strTemp = "";
 
 			if (input[i] == '-')
-			{
-				if(strTemp2.compare("") == 0)
-					strMinus += strTemp1;
-				else
-					strMinus += strTemp2;
-				
-				strMinus += '-';
-				strTemp1 = "";
-				strTemp2 = "";
-			}
-			else if (input[i] == '+')
-			{
-				bPlus = true;
-				strTemp2 = strTemp1;
-				strTemp1 = "";
-			}
+				bMinus = true;
 		}
 		else
-		{
-			strTemp1 += input[i];
-		}
+			strTemp += input[i];
 	}
-
-	if (bPlus)
-	{
-		int nTemp = stoi(strTemp1) + stoi(strTemp2);
-		strMinus += to_string(nTemp);
-	}
+	if (bMinus)
+		nSum -= stoi(strTemp);
 	else
-		strMinus += strTemp1;
-
-	strTemp1 = strMinus[0];
-	bool bFirst = true;
-	int nSum = INF;
-	for (int i = 1; i < strMinus.size(); i++)
-	{
-		if (strMinus[i] == '-')
-		{
-			if (bFirst)
-			{
-				nSum = stoi(strTemp1);
-				bFirst = false;
-			}
-			else
-			{
-				nSum -= stoi(strTemp1);
-			}
-			strTemp1 = "";
-		}
-		else
-		{
-			strTemp1 += strMinus[i];
-		}
-	}
-
-	if (nSum == INF)
-		nSum = stoi(strTemp1);
-	else
-		nSum -= stoi(strTemp1);
+		nSum += stoi(strTemp);
 
 	cout << nSum << '\n';
 
