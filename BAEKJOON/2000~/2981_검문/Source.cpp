0f49ad28e0f5302acadf8286b27b7c97a7a8b5b0
ledpear <tjsrb75@gmail.com>
2021-01-26 12:46:32 +0900

    이항 계수 2 통과

--- BAEKJOON/2000~/2981_검문/Source.cpp
+++ BAEKJOON/2000~/2981_검문/Source.cpp
@@ -9,5 +9,25 @@ using namespace std;
 
 int main()
 {
+	int nSize, nCount;
+	cin >> nSize >> nCount;
+
+	vector<vector<int>> vDP(nSize + 1, vector<int>(nSize + 1));
+
+	for (int i = 0; i <= nSize; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0 || j == i)
+				vDP[i][j] = 1;
+			else
+				vDP[i][j] = (vDP[i - 1][j - 1] + vDP[i - 1][j]) % 10007;
+		}
+	}
+
+	cout << vDP[nSize][nCount] << '\n';
+
+	system("pause");
+
 	return 0;
 }
