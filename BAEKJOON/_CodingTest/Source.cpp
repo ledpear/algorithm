6ac5da3693458b892df1e65c0a69eb7c17fbcff6
ledpear <tjsrb75@gmail.com>
2021-01-29 13:46:27 +0900

    샘플 코드 업데이트

--- BAEKJOON/_CodingTest/Source.cpp
+++ BAEKJOON/_CodingTest/Source.cpp
@@ -4,10 +4,16 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
+#include <stack>
+#include <queue>
+#include <deque>
 
 using namespace std;
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	return 0;
 }
