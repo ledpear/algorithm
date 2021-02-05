07e5067c6d90be0c7e319b6e39589471dcc71510
ledpear <tjsrb75@gmail.com>
2021-02-06 00:13:31 +0900

    codeforce 2번 통과

--- CODEFORCES/Round699/2. New Colony/Source.cpp
+++ CODEFORCES/Round699/2. New Colony/Source.cpp
@@ -15,5 +15,36 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int TestSize;
+	cin >> TestSize;
+	for (int nTest = 0; nTest < TestSize; nTest++)
+	{
+		int nSize, nCount, nResult;
+		cin >> nSize >> nCount;
+		vector<int> vArr(nSize);
+		for (int i = 0; i < nSize; i++)
+			cin >> vArr[i];
+
+		for (int i = 0; i < nCount; i++)
+		{
+			nResult = -1;
+			for (int j = 0; j < nSize - 1; j++)
+			{
+				if (vArr[j] < vArr[j + 1])
+				{
+					vArr[j]++;
+					nResult = j + 1;						
+					break;
+				}
+			}
+			if (nResult == -1)
+				break;
+		}
+
+		cout << nResult << '\n';
+	}
+
+	system("pause");
+
 	return 0;
 }
