0ad1624e7a67e4192f754c18a59265e90e434490
ledpear <tjsrb75@gmail.com>
2021-02-10 12:14:22 +0900

    2110 공유기 설치 브루드포스 실패

--- BAEKJOON/2000~/2110_공유기 설치/Source.cpp
+++ BAEKJOON/2000~/2110_공유기 설치/Source.cpp
@@ -9,6 +9,28 @@
 #include <deque>
 
 using namespace std;
+vector<int> vArr;
+int nResult = 0;
+void bt(int nStart, int nCount, int nMin, int nFind)
+{
+	if (nCount == 0)
+	{
+		nResult = max(nResult, nMin);
+	}
+	else
+	{
+		int nEnd = vArr.size() - nCount;
+		int nTempMin = nMin;
+
+		for (int i = nStart; i <= nEnd; i++)
+		{
+			if (nFind >= 1)
+				nMin = min(nMin, vArr[i] - vArr[nStart - 1]);
+			bt(i + 1, nCount - 1, nMin, nFind + 1);
+			nMin = nTempMin;
+		}
+	}
+}
 
 int main()
 {
@@ -17,11 +39,16 @@ int main()
 
 	int nSize, nCount;
 	cin >> nSize >> nCount;
-	vector<int> vArr(nSize);
+	vArr = vector<int>(nSize);
 	for (int i = 0; i < nSize; i++)
 		cin >> vArr[i];
+	sort(vArr.begin(), vArr.end());
 
+	bt(0, nCount, vArr[nSize - 1], 0);
 
+	cout << nResult << '\n';
+
+	system("pause");
 
 	return 0;
 }
