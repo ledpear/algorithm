50ae2ed34f871428f7a3fd19b82bb0cf70f3bc23
ledpear <tjsrb75@gmail.com>
2021-02-10 11:09:46 +0900

    1654 랜선 자르기 1차시도 실패

---
+++ BAEKJOON/1000~/1654_랜선 자르기/Source.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int nSize, nTarget, nMax = 0;
+	cin >> nSize >> nTarget;
+
+	vector<int> vArr(nSize);
+	for (int i = 0; i < nSize; i++)
+	{
+		cin >> vArr[i];
+		if (nMax < vArr[i])
+			nMax = vArr[i];
+	}
+		
+
+	int nStart, nEnd, nFind, nResult;
+	nStart = 1;
+	nEnd = nMax;
+	nResult = 0;
+	while (nStart <= nEnd)
+	{
+		nFind = (nStart + nEnd) / 2;
+		int nCount = 0;
+		for (int i = 0; i < nSize; i++)
+		{
+			nCount += vArr[i] / nFind;
+		}
+		if (nCount >= nTarget)
+		{
+			nStart = nFind + 1;
+			if (nResult < nFind);
+				nResult = nFind;
+		}
+		else
+		{
+			nEnd = nFind - 1;
+		}
+	}
+
+	cout << nResult << '\n';
+	system("pause");
+
+	return 0;
+}
---
+++ BAEKJOON/2000~/2110_공유기 설치/Source.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int nSize, nCount;
+	cin >> nSize >> nCount;
+	vector<int> vArr(nSize);
+	for (int i = 0; i < nSize; i++)
+		cin >> vArr[i];
+
+
+
+	return 0;
+}
