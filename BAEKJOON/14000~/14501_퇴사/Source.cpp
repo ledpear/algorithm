fb9cf239aeeb9a598e30558b85d11b29ea5143b3
ledpear <tjsrb75@gmail.com>
2021-02-15 12:58:50 +0900

    14501 퇴사 통과

---
+++ BAEKJOON/14000~/14501_퇴사/Source.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+typedef pair<int, int> p;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int nSize;
+	cin >> nSize;
+	vector<p> vArr(nSize);
+	vector<int> vDP(nSize + 1, 0);
+	int nMax = 0;
+	for (int i = 0; i < nSize; i++)
+	{
+		cin >> vArr[i].first >> vArr[i].second;
+	}
+
+	for (int i = 0; i < nSize; i++)
+	{
+		for (int j = i + vArr[i].first; j <= nSize; j++)
+		{
+			vDP[j] = max(vDP[j], vArr[i].second + vDP[i]);
+			if (nMax < vDP[i + vArr[i].first])
+				nMax = vDP[i + vArr[i].first];
+		}
+	}
+
+	cout << nMax << '\n';
+
+	return 0;
+}
