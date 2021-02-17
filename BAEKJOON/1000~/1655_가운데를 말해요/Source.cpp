e26bf8f62abb06de16842dc61a6e33f713eae113
ledpear <tjsrb75@gmail.com>
2021-02-17 22:40:34 +0900

    1655 가운데를 말해요 통과

--- BAEKJOON/1000~/1655_가운데를 말해요/Source.cpp
+++ BAEKJOON/1000~/1655_가운데를 말해요/Source.cpp
@@ -15,91 +15,40 @@ using namespace std;
 typedef unsigned long long ull;
 typedef pair<ull, ull> p;
 
-class cPQ
-{
-public:
-	cPQ() 
-	{
-	}
-	void push(int num) 
-	{
-		vArr.push_back(num);
-		int pos, P_pos;
-		pos = vArr.size() - 1;
-		while (pos != 0)
-		{
-			int log = log2(pos + 1);
-			P_pos = pow(2, log) - 2;
-			if (vArr[pos] < vArr[P_pos])
-			{
-				int temp = vArr[pos];
-				vArr[pos] = vArr[P_pos];
-				vArr[P_pos] = temp;
-
-				sort(pos, P_pos + 1);
-
-				pos = P_pos;
-			}
-			else
-			{
-				sort(pos, P_pos + 1);
-				break;
-			}
-		}
-	}
-	int mid()
-	{
-		return vArr[(vArr.size() - 1) / 2];
-	}
-	
-private:
-	void sort(int pos, int range)
-	{
-		int nVal = vArr[pos];
-		while (true)
-		{
-			if (range == pos)
-			{
-				vArr[range] = nVal;
-				break;
-			}
-			else if (vArr[pos - 1] > nVal)
-			{
-				vArr[pos] = vArr[pos - 1];
-				pos--;
-			}
-			else
-			{
-				vArr[pos] = nVal;
-				break;
-			}
-		}
-	}
-
-	vector<int> vArr;
-};
-
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int nSize, nTemp;
+	int nSize, nTemp, nMid;
 	cin >> nSize;
-	//priority_queue<int, vector<int>, greater<int>> pq;
-	vector<int> vArr;
-// 	cin >> nTemp;
-// 	vArr.push_back(nTemp);
-
-	cPQ cpq;
+	priority_queue<int, vector<int>, greater<int>> pq_Min;
+	priority_queue<int> pq_Max;
+	
 	for (int i = 0; i < nSize; i++)
 	{
 		cin >> nTemp;
 
-		cpq.push(nTemp);
+		if (pq_Min.size() == pq_Max.size())
+			pq_Max.push(nTemp);
+		else
+			pq_Min.push(nTemp);
 
+		if (!pq_Max.empty() && !pq_Min.empty())
+		{
+			if (pq_Max.top() > pq_Min.top())
+			{
+				nTemp = pq_Max.top();
+				pq_Max.pop();
+				pq_Max.push(pq_Min.top());
+				pq_Min.pop();
+				pq_Min.push(nTemp);
+			}
+		}
 
-		cout << cpq.mid() << '\n';
+		cout << pq_Max.top() << '\n';
 	}
+
+
 	system("pause");
 	return 0;
 }
