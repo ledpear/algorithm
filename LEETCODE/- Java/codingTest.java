83fa249b78594ef16999640da7ea89eaf0cef63e
ledpear <tjsrb75@gmail.com>
2021-02-22 11:36:51 +0900

    Leetcode 샘플 코드 업데이트

---
+++ LEETCODE/- Java/codingTest.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class codingTest{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("test");
+    }
+}
+
+class Solution {
+    
+}
