e7971e690c26aba9000984c0dde4bdf857ed4982
ledpear <tjsrb75@gmail.com>
2021-02-23 11:04:48 +0900

    9655 돌 게임 통과 - java

---
+++ BAEKJOON/9000~/9655_돌 게임 - java/codingTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class codingTest{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scn = new Scanner(System.in);
+        int input;
+        input = scn.nextInt();
+        if(input % 2 == 0){
+            System.out.println("CY");
+        }
+        else{
+            System.out.println("SK");
+        }
+    }
+}
+
+class Solution {
+    
+}
