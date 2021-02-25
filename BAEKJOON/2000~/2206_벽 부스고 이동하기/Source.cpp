c6d0ba92f7798b87fe8178cc37132926c7f96ad0
ledpear <tjsrb75@gmail.com>
2021-02-25 18:03:07 +0900

    2206 벽 부수고 이동하기 - DFS 실패

--- BAEKJOON/2000~/2206_벽 부스고 이동하기/Source.cpp
+++ BAEKJOON/2000~/2206_벽 부스고 이동하기/Source.cpp
@@ -14,16 +14,81 @@ using namespace std;
 typedef unsigned long long ull;
 typedef pair<short, short> p;
 typedef pair<p, bool> pos;
-typedef vector<vector<short>> vmap;
+typedef vector<vector<int>> vmap;
+
+void DFS(short nPosX, short nPosY, short count, vmap &vMap, vmap &vScore, bool bCrush)
+{
+	int nX = vMap.size();
+	int nY = vMap[0].size();
+	
+	vScore[nPosX][nPosY] = count;
+
+	if (nPosX == vMap.size() - 1 && nPosY == vMap[0].size() - 1)
+	{
+		return;
+	}
+
+	if (bCrush)	//벽을 이미 부섰을 경우
+	{
+		if (nPosY < nY - 1 && vMap[nPosX][nPosY + 1] == 0 && vScore[nPosX][nPosY + 1] > count + 1)
+			DFS(nPosX, nPosY + 1, count + 1, vMap, vScore, true);
+
+		if (nPosX < nX - 1 && vMap[nPosX + 1][nPosY] == 0 && vScore[nPosX + 1][nPosY] > count + 1)
+			DFS(nPosX + 1, nPosY, count + 1, vMap, vScore, true);
+
+		if (nPosX > 0 && vMap[nPosX - 1][nPosY] == 0 && vScore[nPosX - 1][nPosY] > count + 1)
+			DFS(nPosX - 1, nPosY, count + 1, vMap, vScore, true);
+
+		if (nPosY > 0 && vMap[nPosX][nPosY - 1] == 0 && vScore[nPosX][nPosY - 1] > count + 1)
+			DFS(nPosX, nPosY - 1, count + 1, vMap, vScore, true);
+
+		
+	}
+	else//벽을 부수지 않은 경우
+	{
+		if (nPosY < nY - 1 && vScore[nPosX][nPosY + 1] > count + 1)
+		{
+			if (vMap[nPosX][nPosY + 1] == 0)
+				DFS(nPosX, nPosY + 1, count + 1, vMap, vScore, false);
+			else if (vMap[nPosX][nPosY + 1] == 1)
+				DFS(nPosX, nPosY + 1, count + 1, vMap, vScore, true);
+		}
+		if (nPosX < nX - 1 && vScore[nPosX + 1][nPosY] > count + 1)
+		{
+			if (vMap[nPosX + 1][nPosY] == 0)
+				DFS(nPosX + 1, nPosY, count + 1, vMap, vScore, false);
+			else if (vMap[nPosX + 1][nPosY] == 1)
+				DFS(nPosX + 1, nPosY, count + 1, vMap, vScore, true);
+		}
+
+		if (nPosX > 0 && vScore[nPosX - 1][nPosY] > count + 1)
+		{
+			if (vMap[nPosX - 1][nPosY] == 0)
+				DFS(nPosX - 1, nPosY, count + 1, vMap, vScore, false);
+			else if (vMap[nPosX - 1][nPosY] == 1)
+				DFS(nPosX - 1, nPosY, count + 1, vMap, vScore, true);
+		}
+		
+		if (nPosY > 0 && vScore[nPosX][nPosY - 1] > count + 1)
+		{
+			if (vMap[nPosX][nPosY - 1] == 0)
+				DFS(nPosX, nPosY - 1, count + 1, vMap, vScore, false);
+			else if (vMap[nPosX][nPosY - 1] == 1)
+				DFS(nPosX, nPosY - 1, count + 1, vMap, vScore, true);
+		}
+		
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int nX, nY, nFind = 0;
+	int nX, nY, nFind = 987654321;
 	bool bResult = false;
 	cin >> nX >> nY;
-	vmap vMap = vmap(nX, vector<short>(nY, 0));
+	vmap vMap = vmap(nX, vector<int>(nY, 0));
+	vmap vScore = vmap(nX, vector<int>(nY, 987654321));
 
 	string strTemp;
 	for (int i = 0; i < nX; i++)
@@ -35,92 +100,11 @@ int main()
 		}
 	}
 
-	queue<pos> q;
-	q.push(pos(p(0, 0), false));
-
-	while (!q.empty())
-	{
-		queue<pos> qTemp;
-
-		while (!q.empty()) 
-		{
-			pos posTemp = q.front();
-			q.pop();
-			int nPosX, nPosY;
-			nPosX = posTemp.first.first;
-			nPosY = posTemp.first.second;
-
-			if (nPosX == nX - 1 && nPosY == nY - 1)
-			{
-				bResult = true;
-				break;
-			}
-
-			vMap[nPosX][nPosY] = -1;
-
-			if (posTemp.second)	//벽을 이미 부섰을 경우
-			{
-				if (nPosX > 0 && vMap[nPosX - 1][nPosY] == 0)
-					qTemp.push(pos(p(nPosX - 1, nPosY), true));
-				if (nPosX < nX - 1 && vMap[nPosX + 1][nPosY] == 0)
-					qTemp.push(pos(p(nPosX + 1, nPosY), true));
-
-				if (nPosY > 0 && vMap[nPosX][nPosY - 1] == 0)
-					qTemp.push(pos(p(nPosX, nPosY - 1), true));
-				if (nPosY < nY - 1 && vMap[nPosX][nPosY + 1] == 0)
-					qTemp.push(pos(p(nPosX, nPosY + 1), true));
-			}
-			else//벽을 부수지 않은 경우
-			{
-				if (nPosX > 0)
-				{
-					if (vMap[nPosX - 1][nPosY] == 0)
-						qTemp.push(pos(p(nPosX - 1, nPosY), false));
-					else if (vMap[nPosX - 1][nPosY] == 1)
-						qTemp.push(pos(p(nPosX - 1, nPosY), true));
-				}
-				if (nPosX < nX - 1)
-				{
-					if (vMap[nPosX + 1][nPosY] == 0)
-						qTemp.push(pos(p(nPosX + 1, nPosY), false));
-					else if (vMap[nPosX + 1][nPosY] == 1)
-						qTemp.push(pos(p(nPosX + 1, nPosY), true));
-				}
-				if (nPosY > 0)
-				{
-					if (vMap[nPosX][nPosY - 1] == 0)
-						qTemp.push(pos(p(nPosX, nPosY - 1), false));
-					else if (vMap[nPosX][nPosY - 1] == 1)
-						qTemp.push(pos(p(nPosX, nPosY - 1), true));
-				}
-				if (nPosY < nY - 1)
-				{
-					if (vMap[nPosX][nPosY + 1] == 0)
-						qTemp.push(pos(p(nPosX, nPosY + 1), false));
-					else if (vMap[nPosX][nPosY + 1] == 1)
-						qTemp.push(pos(p(nPosX, nPosY + 1), true));
-				}
-			}
-		}
-
-		if (qTemp.empty())
-		{
-			if (bResult)
-			{
-				cout << nFind + 1 << '\n';
-				break;
-			}
-			else
-			{
-				cout << "-1\n";
-				break;
-			}
-		}			
-		else
-			q = qTemp;
-
-		nFind++;
-	}
+	DFS(0, 0, 1, vMap, vScore, false);
+	if (vScore[nX - 1][nY - 1] == 987654321)
+		cout << "-1\n";
+	else
+		cout << vScore[nX - 1][nY - 1] << '\n';
 
 	system("pause");
 	return 0;
