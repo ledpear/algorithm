5108c393f15040d5d1a9cbb8c9fdb35ef73ad2b1
ledpear <tjsrb75@gmail.com>
2021-03-04 10:20:40 +0900

    11057 오르막 수 통과 : DP

--- BAEKJOON/11000~/11057_오르막 수/Source.cpp
+++ BAEKJOON/11000~/11057_오르막 수/Source.cpp
@@ -19,26 +19,22 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	int nSize;
-	cin >> nSize;
-	vector<int> vArr(nSize + 1);
-	vector<int> vDP(nSize + 1, 0);
-	
-	for (int i = 1; i <= nSize; i++)
+	int nInput;
+	cin >> nInput;
+	vector<int> vDP(10);
+	for (int i = 0; i < 10; i++)
 	{
-		cin >> vArr[i];
+		vDP[i] = i + 1;
 	}
-
-	for (int i = 1; i <= nSize; i++)
+	for (int i = 1; i < nInput; i++)
 	{
-		for (int j = i; j <= nSize; j++)
+		for (int j = 1; j < 10; j++)
 		{
-			vDP[j] = max(vDP[j], vArr[i] + vDP[j - i]);
+			vDP[j] = (vDP[j] + vDP[j - 1]) % 10007;
 		}
 	}
 
-	cout << vDP[nSize]<< '\n';
+	cout << vDP[9] << '\n';
 	system("pause");
 
 	return 0;
