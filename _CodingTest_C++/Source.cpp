322c367d9271f86dad744c4ded88cadb0ed65fda
ledpear <tjsrb75@gmail.com>
2021-03-06 19:04:17 +0900

    샘플 코드 업데이트
    디파인 추가
    커스텀 템플릿 함수 추가(테스트용)
    system("pause")추가

--- _CodingTest_C++/Source.cpp
+++ _CodingTest_C++/Source.cpp
@@ -11,14 +11,33 @@
 #include <set>
 
 using namespace std;
+
+//define
+#define DEF_MIN -2147483648;
+#define DEF_MAX 2147483647;
+
 typedef unsigned long long ull;
 typedef pair<ull, ull> p;
 typedef vector<vector<int>> vmap;
+
+//custum function
+template <typename T>
+void tSwap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	////////////////////////////////////
 
+
+
+	////////////////////////////////////
+	system("pause");
 	return 0;
 }
