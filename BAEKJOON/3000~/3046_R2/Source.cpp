b5391f87dbe33d0197173b090c4c80616a41ae60
ledpear <tjsrb75@gmail.com>
2021-03-15 21:40:42 +0900

    3046 R2 통과 : 단순구현

---
+++ BAEKJOON/3000~/3046_R2/Source.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+
+using namespace std;
+
+//define
+#define DEF_MIN -2147483648
+#define DEF_MAX 2147483647
+
+typedef unsigned long long ull;
+typedef pair<ull, ull> p;
+typedef vector<vector<int>> vmap;
+
+//custum function
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	////////////////////////////////////
+	//Declaration
+	int R1, S;
+
+	//Input
+	cin >> R1 >> S;
+
+	//Solution
+	//Output
+	cout << 2 * S - R1 << '\n';
+
+	////////////////////////////////////
+	system("pause");
+	return 0;
+}
