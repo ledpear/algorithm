69a8e9b3792fc7e448708cd6da8323d2146273cc
ledpear <tjsrb75@gmail.com>
2021-03-16 15:52:46 +0900

    1100 하얀 칸 통과 : 단순구현

---
+++ BAEKJOON/1000~/1100_하얀 칸/Source.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+
+using namespace std;
+
+//define
+#define DEF_MIN -2147483648
+#define DEF_MAX 2147483647
+
+typedef unsigned long long ull;
+typedef pair<ull, ull> p;
+typedef vector<vector<int>> vmap;
+
+//custum function
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	////////////////////////////////////
+	//Declaration
+	int nCount = 0;
+
+	//Input
+	//Solution
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			char temp;
+			cin >> temp;
+			if ((i + j) % 2 == 0 && temp == 'F')
+				nCount++;
+		}
+	}
+
+	//Output
+	cout << nCount << '\n';;
+
+	////////////////////////////////////
+	system("pause");
+	return 0;
+}
