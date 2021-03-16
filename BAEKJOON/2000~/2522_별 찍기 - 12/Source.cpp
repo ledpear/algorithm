dbdbf14ca953400b72631a96a16e0b8905680d6a
ledpear <tjsrb75@gmail.com>
2021-03-16 12:44:32 +0900

    2522 별 찍기 -12 통과 : 단순구현

---
+++ BAEKJOON/2000~/2522_별 찍기 - 12/Source.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+
+using namespace std;
+
+//define
+#define DEF_MIN -2147483648
+#define DEF_MAX 2147483647
+
+typedef unsigned long long ull;
+typedef pair<ull, ull> p;
+typedef vector<vector<int>> vmap;
+
+//custum function
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	////////////////////////////////////
+	//Declaration
+	int nInput, nSize;
+
+	//Input
+	cin >> nInput;
+
+	//Solution
+	nSize = nInput * 2 - 1;
+
+	for (int i = 1; i <= nSize; i++)
+	{
+		string strTemp = "";
+		int nStar = i;
+
+		if (i > nInput)
+			nStar = nSize - i + 1;
+
+		for (int j = 0; j < nInput - nStar; j++)
+			strTemp += ' ';
+		for (int j = 0; j < nStar; j++)
+			strTemp += '*';
+
+		//Output
+		cout << strTemp << '\n';
+	}
+
+	////////////////////////////////////
+	system("pause");
+	return 0;
+}
