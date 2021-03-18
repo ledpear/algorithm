7d734e94c4825915a2bc9565f85270da0f45b73f
ledpear <tjsrb75@gmail.com>
2021-03-18 17:50:24 +0900

    14502 연구소 통과 : BFS, 시뮬레이션, 브루트포스

--- BAEKJOON/14000~/14502_연구소/Source.cpp
+++ BAEKJOON/14000~/14502_연구소/Source.cpp
@@ -23,6 +23,7 @@ typedef vector<vector<int>> vmap;
 //custum function
 int BFS(vmap& vMap, int nX, int nY)
 {
+	int nCount = 0;
 	int nSizeX = vMap.size();
 	int nSizeY = vMap[0].size();
 
@@ -31,25 +32,48 @@ int BFS(vmap& vMap, int nX, int nY)
 	
 	while (qPoint.empty() == false)
 	{
-		const int nPosX = qPoint.back().first;
-		const int nPosY = qPoint.back().second;
-		const int nValue = vMap[nPosX][nPosY];
+		const int nNowX = qPoint.front().first;
+		const int nNowY = qPoint.front().second;
+		const int nValue = vMap[nNowX][nNowY];
 		qPoint.pop();
-		if(nValue != 0)	continue;
 
 		//이동패턴 정의
 		int nMoveX[4] = { 0, 0, 1, -1 };
 		int nMoveY[4] = { 1, -1, 0, 0 };
 
+		//이동할 수 있는 곳은 이동
+		for (int i = 0; i < 4; i++)
+		{
+			int nPosX = nNowX + nMoveX[i];
+			int nPosY = nNowY + nMoveY[i];
 
+			//범위 및 방문 예외처리
+			if (nPosX >= 0 && nPosX < nSizeX && nPosY >= 0 && nPosY < nSizeY && vMap[nPosX][nPosY] == 0)
+			{
+				//조건
+				++nCount;
+				vMap[nPosX][nPosY] = 2;
+				qPoint.push(p(nPosX, nPosY));
+			}
+		}
 	}
+
+	return nCount;
 }
 
-void BackTracking(vmap& vMap, const vector<p>& vVirus, int &nBlank, int &nMax, int nCount = 0)
+void BackTracking(vmap& vMap, const vector<p>& vVirus, int nBlank, int &nMax, int nCount = 0)
 {
 	if (nCount == 3)
 	{
 		vmap vSave = vMap;
+		for (int i = 0; i < vVirus.size(); i++)
+		{
+			int nVirus = BFS(vSave, vVirus[i].first, vVirus[i].second);
+			nBlank -= nVirus;
+		}
+
+		nMax = max(nMax, nBlank);
+		return;
 	}
 
 	int nSizeX = vMap.size();
@@ -95,10 +119,10 @@ int main()
 	}
 
 	//Solution
-
+	BackTracking(vMap, vVirus, nBlank, nMax);
 
 	//Output
-
+	cout << nMax - 3 << '\n';
 
 	////////////////////////////////////
 	system("pause");
