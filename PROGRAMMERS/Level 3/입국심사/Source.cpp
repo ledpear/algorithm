98c8adc17236db7a4c935b53a6674c196fd9d2ba
ledpear <tjsrb75@gmail.com>
2021-03-19 21:55:24 +0900

    레벨3 입국심사 추가만 함

---
+++ PROGRAMMERS/Level 3/입국심사/Source.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//define
+#define DEF_MIN -2147483648
+#define DEF_MAX 2147483647
+
+using ull = unsigned long long ;
+using location =  pair<int, int> ;
+using matrix = vector<vector<int>>;
+
+//custum function
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	////////////////////////////////////
+	//Declaration
+
+
+	//Solution
+
+
+	//Output
+
+
+	////////////////////////////////////
+	system("pause");
+	return 0;
+}
