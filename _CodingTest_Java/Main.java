999cda67bf156821c62f6da4bc6116e4164bf8a3
ledpear <tjsrb75@gmail.com>
2021-03-22 13:42:23 +0900

    Java 샘플코드 수정

---
+++ _CodingTest_Java/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        Solution solution = new Solution();
+    }
+}
+
+class Solution {
+    
+}
