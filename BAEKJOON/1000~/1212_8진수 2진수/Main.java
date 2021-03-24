e86e17050d70cfa42440026179714be05f3befd7
ledpear <tjsrb75@gmail.com>
2021-03-24 09:48:04 +0900

    1212 8진수 2진수 통과 : 구현, 문자열

---
+++ BAEKJOON/1000~/1212_8진수 2진수/Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static char[] twoCharArr;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String eight = br.readLine();
+		char[] eightCharArr = eight.toCharArray();
+		
+		int twoLen = getPossibleDigitNumber(eightCharArr);
+		twoCharArr = new char[twoLen];
+		
+		int index = twoLen - 1;
+		// 가장 첫 번째 자리(맨뒤의 수) 부터 탐색 시작 
+		for(int i = eight.length() - 1; i >= 0; i--) {
+			char num = eightCharArr[i];
+			index = getTwoDigitNumber(num, index);
+		}
+		
+		System.out.print(new String(twoCharArr));
+    }
+
+    static int getTwoDigitNumber(char ch, int nowIndex) {
+		int num = ch - '0';
+		
+		// 총 3회 반복 
+		for(int i = 0; i < 3; i++) {
+			twoCharArr[nowIndex--] = (char) ((num % 2) + '0');
+			num /= 2;
+			
+			if(nowIndex < 0) { // 이미 전체 2진수개수를 알고 있으므로 index로 길이 판단.
+				break;
+			}
+		}
+		
+		return nowIndex;
+	}
+	
+	// 8진수를 2진수로 변환했을 때의 2진수의 길이 리턴 
+	static int getPossibleDigitNumber(char[] charArr) {
+		int len = charArr.length * 3;
+		if(len == 0) {
+			return 0;
+		}
+		
+		int firstNum = charArr[0] - '0';
+		if(firstNum / 4 > 0) { // 첫 번째 숫자가 3자리 수 가능 
+			return len;
+		}
+		if(firstNum / 2 > 0) { // 첫 번째 숫자가 2자리 수 가능
+			return len - 1;
+		}
+		
+		return len - 2;
+	}
+}
