1ff8435991d426366dc3a46cf419ea99ec419820
ledpear <tjsrb75@gmail.com>
2021-04-23 12:15:18 +0900

    1309 동물원 파이썬 예외처리

--- BAEKJOON/1000~/1309_동물원/CodingTest.py
+++ BAEKJOON/1000~/1309_동물원/CodingTest.py
@@ -11,4 +11,4 @@ for i in range(2, N + 1):
     arr[i][1] = (arr[i - 1][0] + arr[i - 1][2]) % 9901
     arr[i][2] = (arr[i - 1][0] + arr[i - 1][1]) % 9901
 
-print(arr[N][0] + arr[N][1] + arr[N][2])
+print((arr[N][0] + arr[N][1] + arr[N][2]) % 9901)
