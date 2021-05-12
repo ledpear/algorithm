ceb8a00ccdc129edf4765243a4bcbb6e687a2a39
ledpear <tjsrb75@gmail.com>
2021-05-12 12:40:16 +0900

    레벨2 최솟값 만들기 파이썬 : 정렬

---
+++ PROGRAMMERS/Level 2/최솟값 만들기/CodingTest.py
@@ -0,0 +1,14 @@
+def solution(A,B):
+    answer = 0
+
+    A = sorted(A)
+    B = sorted(B, reverse = True)
+
+    size = len(A)
+
+    for i in range(size):
+        answer += A[i] * B[i]
+
+    return answer
+
+print(solution([1, 4, 2], [5, 4, 4]))
