2b11c7840427e1258d71f4e628d50285896cbfdb
ledpear <tjsrb75@gmail.com>
2021-06-01 20:55:07 +0900

    17780 새로운 게임 파이썬 통과 : 구현

--- BAEKJOON/17000~/17780_새로운 게임_python/CodingTest.py
+++ BAEKJOON/17000~/17780_새로운 게임_python/CodingTest.py
@@ -33,62 +33,85 @@ for _ in range(map_size):
 
 piece_info = [] #x, y, dir
 
-for i in range(map_size):
+for i in range(piece_size):
     x, y, dir = map(int, input().split())
-    piece_info[i].append([[x -1, y - 1], dir, True])
+    x -= 1
+    y -= 1
+    piece_info.append([[x, y], dir, True])
     chessboard[x][y][0].append(i)
     chessboard[x][y][1] += 1
 
-count = 0
-
+count = 1
+result = False
 #def move(piece_index):
 
-while count < 1000:
-    count += 1
-
+while count <= 1000:
     for piece in piece_info:
         #파란바닥 : 방향을 반대로하고 이동. 단, 이동하는 칸도 파란바닥일 경우 방향만 반대로
-        #빨간바닥 : 내부 피스를 로테이션 후 이동
+        #빨간바닥 : 내부 피스의 방향을 반대로
         #이동 : 그냥 이동과 합치는 경우로 나뉨
         #파란바닥 처리 후 이동 처리
         #빨간바닥 처리 후 이동 처리
         if not piece[2]: continue
 
-        x, y, dir, able = piece
+        pos, dir, able = piece
+        x = pos[0]
+        y = pos[1]
         pos_x = x + move_x[dir]
         pos_y = y + move_y[dir]
 
-        if pos_x >= 0 and pos_y >= 0 and pos_x < map_size and pos_y < map_size:
-            #파란 바닥 처리
-            if map_info[pos_x][pos_y] == 2 :
-                piece[2] = Reverse(dir)
-            #빨간 and 하얀 바닥 처리
-            else:
-                #빨간 바닥 처리
-                if map_info[pos_x][pos_y] == 1 and chessboard[x][y][1] > 1:
-                    chessboard[x][y][0].append(chessboard[x][y][0].pop(0))
-                    piece_info[chessboard[x][y][0][0]][2] = True
-                    piece_info[chessboard[x][y][0][-1]][2] = False
+        #맵 외곽 처리 - 파란 바닥과 동일
+        if pos_x < 0 or pos_y < 0 or pos_x >= map_size or pos_y >= map_size:
+            piece[1] = Reverse(dir)
+            dir = piece[1]
 
-                #하얀 바닥 처리 : 합치는 경우와 이동만 하는 경우
-                if chessboard[pos_x][pos_y][1] >= 1:#합치는 경우
-                    chessboard[pos_x][pos_y][0].extend(chessboard[x][y][0])
-                    chessboard[pos_x][pos_y][1] += 1
-                    if chessboard[pos_x][pos_y][1] >= 4:
-                        break;
-                    chessboard[x][y][0].clear()
-                    chessboard[x][y][1] = 0
-                    piece[3] = False
+            pos_x = x + move_x[dir]
+            pos_y = y + move_y[dir]
 
-                else:
-                    chessboard[pos_x][pos_y] = deepcopy(chessboard[x][y])
-                    chessboard[x][y][0].clear()
-                    chessboard[x][y][1] = 0
-                    piece[0] = pos_x
-                    piece[1] = pos_y
+            if pos_x < 0 or pos_y < 0 or pos_x >= map_size or pos_y >= map_size or map_info[pos_x][pos_y] == 2:
+                continue
+                
+
+        #파란 바닥 처리
+        if map_info[pos_x][pos_y] == 2 :
+            piece[1] = Reverse(dir)
+            dir = piece[1]
+
+            pos_x = x + move_x[dir]
+            pos_y = y + move_y[dir]
+
+            if pos_x < 0 or pos_y < 0 or pos_x >= map_size or pos_y >= map_size or map_info[pos_x][pos_y] == 2:
+                continue
+
+        #빨간 바닥 처리
+        if map_info[pos_x][pos_y] == 1 and chessboard[x][y][1] > 1:
+            chessboard[x][y][0].reverse()
+            piece_info[chessboard[x][y][0][0]][2] = True
+            piece_info[chessboard[x][y][0][-1]][2] = False
+
+        #하얀 바닥 처리 : 합치는 경우와 이동만 하는 경우
+        if chessboard[pos_x][pos_y][1] >= 1:#합치는 경우
+            chessboard[pos_x][pos_y][0].extend(chessboard[x][y][0])
+            chessboard[pos_x][pos_y][1] += chessboard[x][y][1]
+            if chessboard[pos_x][pos_y][1] >= 4:
+                result = True
+                break
+            #piece_info[chessboard[x][y][0][0]][0] = piece_info[chessboard[pos_x][pos_y][0][0]][0]
+            piece_info[chessboard[x][y][0][-1]][0] = piece_info[chessboard[pos_x][pos_y][0][0]][0]
+            piece_info[chessboard[x][y][0][0]][2] = False
+            chessboard[x][y][0].clear()
+            chessboard[x][y][1] = 0
         else:
-            #외각 처리 = 파란 바닥 처리
-            piece[2] = Reverse(dir)
+            chessboard[pos_x][pos_y] = deepcopy(chessboard[x][y])
+            piece_info[chessboard[x][y][0][0]][0][0] = pos_x
+            piece_info[chessboard[x][y][0][0]][0][1] = pos_y
+            chessboard[x][y][0].clear()
+            chessboard[x][y][1] = 0
+
+    if result :
+        break
+    else:
+        count += 1
 
 if count <= 1000:
     print(count)
