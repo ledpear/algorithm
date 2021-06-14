cc288b21655265eb947a6c3fc16de755d309d72b
ledpear <tjsrb75@gmail.com>
2021-06-14 10:50:31 +0900

    1202 보석 도둑 파이썬 실패 : 힙정렬

--- BAEKJOON/1000~/1202_보석 도둑_python/CodingTest.py
+++ BAEKJOON/1000~/1202_보석 도둑_python/CodingTest.py
@@ -1,19 +1,37 @@
 import string
 import sys
+import heapq
 
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
-n_arr = []
+n_arr = [[] for _ in range(1000001)]
 k_arr = []
 
 for _ in range(n):
-    n_arr.append(list(map(int, input().split())))
+    w, v = map(int, input().split())
+    heapq.heappush(n_arr[w], -v)
+
 for _ in range(k):
-    k_arr.append(int(input().rstrip()))
+    heapq.heappush(k_arr, int(input().rstrip()))
 
-n_arr.sort()
-k_arr.sort()
+answer = 0
 
-print(n_arr)
+while k_arr:
+    now_w = heapq.heappop(k_arr)
+
+    maxVal = 0
+    maxIndex = 0
+
+    for i in range(1, now_w + 1):
+        if not n_arr[i]:
+            continue
+        if maxVal < -n_arr[i][0]:
+            maxVal = -n_arr[i][0]
+            maxIndex = i
+    
+    if maxIndex != 0:
+        answer += maxVal
+        heapq.heappop(n_arr[maxIndex])
+
+print(answer)
