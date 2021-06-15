2fcb0550b28ab0c7e8cf66469f765aaa5c843a19
ledpear <tjsrb75@gmail.com>
2021-06-15 14:12:50 +0900

    1800 인터넷 설치 파이썬 통과 : 다익스트라 + 이분탐색

--- BAEKJOON/1000~/1800_인터넷 설치_python/CodingTest.py
+++ BAEKJOON/1000~/1800_인터넷 설치_python/CodingTest.py
@@ -5,6 +5,35 @@ import heapq
 INF = 1e9
 input = sys.stdin.readline
 
+def dijkstra(start, limit):
+    visit = [INF] * (node_size + 1)
+    visit[start] = 0
+    heap_list = []
+    heapq.heappush(heap_list, [1,0])
+
+    while heap_list:
+        now_node, now_cost = heapq.heappop(heap_list)
+
+        if now_cost > visit[now_node]:
+            continue
+
+        for edge in edges[now_node]:
+            target_node, target_weight = edge
+            
+            if target_weight <= limit:
+                if visit[target_node] > now_cost:
+                    visit[target_node] = now_cost
+                    heapq.heappush(heap_list, [target_node, now_cost])
+            else:
+                if visit[target_node] > now_cost + 1:
+                    visit[target_node] = now_cost + 1
+                    heapq.heappush(heap_list, [target_node, now_cost + 1])
+
+    if visit[node_size] <= free_size:
+        return True
+    else:
+        return False
+
 node_size, edge_size, free_size = map(int, input().split())
 
 edges = [[] for _ in range(node_size + 1)]
@@ -13,22 +42,20 @@ for i in range(edge_size):
     edges[nodeA].append([nodeB, cost])
     edges[nodeB].append([nodeA, cost])
 
-cost_arr = [INF] * (node_size + 1)
-heap_list = []
-heapq.heappush(heap_list, [1,0])
+start = 0
+end = 1000001
+answer = INF
 
+while start <= end:
+    mid = (start + end) //2
 
-while heap_list:
-    now_node, now_cost = heapq.heappop(heap_list)
+    if dijkstra(1, mid):
+        end = mid - 1
+        answer = mid
+    else:
+        start = mid + 1
 
-    if cost_arr[now_node] <= now_cost:
-        continue
-
-    cost_arr[now_node] = now_cost
-
-    for edge in edges[now_node]:
-        target_node, target_cost = edge
-        
-        heapq.heappush(heap_list, [target_node, target_cost + now_cost])
-
-print(cost_arr[node_size])
+if answer == INF:
+    print(-1)
+else:
+    print(answer)
