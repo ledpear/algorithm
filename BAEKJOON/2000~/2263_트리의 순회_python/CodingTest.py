f3dc490d2c1e6a1de906fa6a47c2f82cef6de150
ledpear <tjsrb75@gmail.com>
2021-07-03 23:36:46 +0900

    2263 트리의 순회 파이썬 진행중

---
+++ BAEKJOON/2000~/2263_트리의 순회_python/CodingTest.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+
+class oper_tree:
+    def __init__(self, left, right, val):
+        self.left = left
+        self.right = right
+        self.val = val
+
+size = int(input().rstrip())
+in_order = list(map(int, input().split()))
+post_order = list(map(int, input().split()))
+
+def makeTree(in_start, in_end, post_start, post_end):
+    if in_start > in_end or post_start > post_end:
+        return None
+
+    root = post_order[post_end]
+    pos = in_order.index(root)
+
+    left_size = pos - in_start
+    right_size = in_end - pos
+
+    left = None
+    right = None
+
+    if pos - 1 >= 0 :
+        left = makeTree(in_start, pos - 1, post_start, post_start + left_size - 1)
+
+    if pos + 1 < size:
+        right = makeTree(pos + 1, in_end, post_end - right_size, post_end - 1)
+
+    return oper_tree(left, right, root)
+
+def printTree(operTree):
+    if operTree == None:
+        return
+    else:
+        print(operTree.val, end = " ")
+
+    printTree(operTree.left)
+    printTree(operTree.right)
+
+tempTree = makeTree(0, size - 1, 0, size - 1)
+printTree(tempTree)
