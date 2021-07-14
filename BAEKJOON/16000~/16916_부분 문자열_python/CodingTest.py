9cb7a95fd5ce151d7dbf8daa8c8d242290eab896
ledpear <tjsrb75@gmail.com>
2021-07-14 16:44:07 +0900

    16916 부분 문자열 파이썬 변수 수정

--- BAEKJOON/16000~/16916_부분 문자열_python/CodingTest.py
+++ BAEKJOON/16000~/16916_부분 문자열_python/CodingTest.py
@@ -6,52 +6,52 @@ input = sys.stdin.readline
 #n, m = map(int, input().split())
 #k = input().rstrip()
 
-input_str = input().rstrip()
+input_mainstr = input().rstrip()
 input_substr = input().rstrip()
 
-def KMPSearch(pat, txt):
-    result = False
-    M = len(pat)
-    N = len(txt)
+def KMPSearch(input_substr, input_mainstr):
+    result = 0
+    subSize = len(input_substr)
+    mainSize = len(input_mainstr)
 
-    lps = [0]*M
+    lps = [0]*subSize
 
-    # Preprocess the pattern
-    computeLPS(pat, lps)
+    # Preprocess the input_substrtern
+    computeLPS(input_substr, lps)
 
-    i = 0  # index for txt[]
-    j = 0  # index for pat[]
-    while i < N:
+    mainIndex = 0  # index for input_mainstr[]
+    subIndex = 0  # index for input_substr[]
+    while mainIndex < mainSize:
         # 문자열이 같은 경우 양쪽 인덱스를 모두 증가시킴
-        if pat[j] == txt[i]:
-            i += 1
-            j += 1
-        # Pattern을 찾지 못한 경우
-        elif pat[j] != txt[i]:
+        if input_substr[subIndex] == input_mainstr[mainIndex]:
+            mainIndex += 1
+            subIndex += 1
+        # input_substrtern을 찾지 못한 경우
+        elif input_substr[subIndex] != input_mainstr[mainIndex]:
             # j!=0인 경우는 짧은 lps에 대해 재검사
-            if j != 0:
-                j = lps[j-1]
+            if subIndex != 0:
+                subIndex = lps[subIndex-1]
             # j==0이면 일치하는 부분이 없으므로 인덱스 증가
             else:
-                i += 1
+                mainIndex += 1
 
-        # Pattern을 찾은 경우
-        if j == M:
-            result = True
-            return result
+        # input_substrtern을 찾은 경우
+        if subIndex == subSize:
+            result += 1
+            #return result
             # 이전 인덱스의 lps값을 참조하여 계속 검색
-            j = lps[j-1]
+            subIndex = lps[subIndex-1]
 
     return result
 
-def computeLPS(pat, lps):
+def computeLPS(input_substr, lps):
     leng = 0  # length of the previous longest prefix suffix
 
     # 항상 lps[0]==0이므로 while문은 i==1부터 시작한다.
     i = 1
-    while i < len(pat):
+    while i < len(input_substr):
         # 이전 인덱스에서 같았다면 다음 인덱스만 비교하면 된다.
-        if pat[i] == pat[leng]:
+        if input_substr[i] == input_substr[leng]:
             leng += 1
             lps[i] = leng
             i += 1
@@ -66,7 +66,4 @@ def computeLPS(pat, lps):
                 lps[i] = 0
                 i += 1
 
-if KMPSearch(input_substr, input_str):
-    print(1)
-else:
-    print(0)
+print(KMPSearch(input_substr, input_mainstr))
