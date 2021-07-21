4d01c09711f5edf2a946a60367fdc963d8a4351f
ledpear <tjsrb75@gmail.com>
2021-07-21 11:12:29 +0900

    15654 N과 M(5) 파이썬 통과 : 직접 구현 - 백트래킹, 순열 조합

--- BAEKJOON/15000~/15654_N과 M(5)_python/CodingTest.py
+++ BAEKJOON/15000~/15654_N과 M(5)_python/CodingTest.py
@@ -1,6 +1,5 @@
 import string
 import sys
-from itertools import permutations
 
 input = sys.stdin.readline
 
@@ -8,9 +7,18 @@ arrSize, permutationSize = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
 
-permutationArr = list(permutations(arr, permutationSize))
+def permutationsPrint(arr, size, result, used):
+    if len(result) == size:
+        for num in result:
+            print(num, end=' ')
+        print()
+    
+    for index in range(len(arr)):
+        if not used[index]:
+            used[index] = True
+            result.append(arr[index])
+            permutationsPrint(arr, size, result, used)
+            result.pop()
+            used[index] = False
 
-for permutation in permutationArr:
-    for num in permutation:
-        print(num, end=' ')
-    print()
+permutationsPrint(arr, permutationSize, [], [False] * arrSize)
