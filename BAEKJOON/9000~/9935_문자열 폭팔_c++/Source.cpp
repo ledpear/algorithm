c289f18612688bf9fb90fd02bb6e18b5ab99a611
ledpear <tjsrb75@gmail.com>
2021-07-24 23:56:09 +0900

    9935 문자열 폭발 C++ 통과 : 문자열

--- BAEKJOON/9000~/9935_문자열 폭팔_c++/Source.cpp
+++ BAEKJOON/9000~/9935_문자열 폭팔_c++/Source.cpp
@@ -21,91 +21,35 @@ using location =  pair<int, int> ;
 using matrix = vector<vector<int>>;
 
 //custum function
-
-vector<int> getPiArr(string subStr) 
-{
-	int size(subStr.size());
-	vector<int> piArr(size);
-
-	int index = 1;
-
-	int sameCount = 0;
-
-	while (index < size)
-	{
-		if (subStr[index] == subStr[sameCount])
-		{
-			++sameCount;
-			piArr[index] = sameCount;
-			++index;
-		}
-		else if (sameCount != 0)
-		{
-			sameCount = piArr[sameCount - 1];
-		}
-		else
-		{
-			++index;
-		}
-	}
-
-	return piArr;
-}
-
 string solution(string mainStr, string subStr)
 {
 	int mainSize = mainStr.size();
 	int subSize = subStr.size();
 
-	//vector<int> subPiArr = getPiArr(subStr);
-	vector<int> mainPiArr(mainSize + 1);
 	//main을 복사한 result를 폭탄이 터졌을때 인덱스를 이동시키는 것으로 속도를 향상시킨다.
 	string resultStr(mainStr);
-	int mainIndex(0), subIndex(0), resultIndex(0);
+	int mainIndex(0), resultIndex(0);
 
-	while (mainIndex < mainSize)
+	for(mainIndex; mainIndex < mainSize; ++mainIndex)
 	{
-		if (mainStr[mainIndex] == subStr[subIndex])
+		resultStr[resultIndex] = mainStr[mainIndex];
+		if (resultIndex >= subSize - 1 && mainStr[mainIndex] == subStr[subSize - 1])
 		{
-			//인덱스가 달랐을때만 복사할까?
-			resultStr[resultIndex] = mainStr[mainIndex];
-			++resultIndex;
-			++subIndex;
-			++mainIndex;
-			mainPiArr[mainIndex] = subIndex;
-
-			if (subIndex == subSize)
+			bool same = true;
+			for (int subIndex(0); subIndex < subSize; ++subIndex)
 			{
-				//resultStr = resultStr.substr(0, resultStr.size() - subSize);
-				//subIndex = subPiArr[subIndex - 1];
-				
-				//기억한 위치값으로 변경
-				subIndex = mainPiArr[resultIndex - subSize];
-				//폭탄이 터지면 sub의 크기만큼 빼준다
+				if (subStr[subIndex] != resultStr[resultIndex - (subSize - subIndex) + 1])
+				{
+					same = false;
+					break;
+				}
+			}
+			if (same)
+			{
 				resultIndex -= subSize;
 			}
 		}
-		else if (subIndex != 0)
-		{
-			//subIndex = subPiArr[subIndex - 1];
-// 			int backPos = mainIndex - subSize - 1;
-// 			if (backPos >= 0)
-// 			{
-// 				subIndex = mainPiArr[mainIndex - subSize - 1];
-// 			}
-// 			else
-// 			{
-// 				subIndex = 0;
-// 			}
-			subIndex = 0;
-		}
-		else
-		{
-			//resultStr += mainStr[mainIndex];
-			resultStr[resultIndex] = mainStr[mainIndex];
-			++resultIndex;
-			++mainIndex;
-		}
+		++resultIndex;
 	}
 
 	return resultStr.substr(0, resultIndex);
