6584636d79acd5875af13409a961a6d039a7c185
ledpear <tjsrb75@gmail.com>
2021-07-30 12:02:25 +0900

    17070 파이프 옮기기 1 C++ : solution 클래스화

--- BAEKJOON/17000~/17070_파이프 옮기기 1_c++/Source.cpp
+++ BAEKJOON/17000~/17070_파이프 옮기기 1_c++/Source.cpp
@@ -1,15 +1,5 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <deque>
 #include <iostream>
-#include <limits>
-#include <map>
 #include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <sstream>
 #include <vector>
 
 using namespace std;
@@ -20,53 +10,115 @@ using ull = unsigned long long ;
 using location =  pair<int, int> ;
 using matrix = vector<vector<int>>;
 
-class Pipe
-{
-public:
-	location pipeFront, pipeRear;
-	Pipe()
-	{
-		pipeFront = { 0, 0 };
-		pipeRear = { 0, 1 };
-	}	
-};
-
 //custum function
-void goRight(matrix& inputMatrix, queue<Pipe>& BFS_Queue, Pipe nowPipe, int mapSize)
+class Solution
 {
-	if (nowPipe.pipeRear.second + 1 < mapSize && 
-		inputMatrix[nowPipe.pipeRear.first][nowPipe.pipeRear.second + 1] == 0)
+	class Pipe
 	{
-		nowPipe.pipeFront = nowPipe.pipeRear;
-		++nowPipe.pipeRear.second;
-		BFS_Queue.push(nowPipe);
-	}
-}
+	public:
+		location pipeFront, pipeRear;
+		Pipe()
+		{
+			pipeFront = { 0, 0 };
+			pipeRear = { 0, 1 };
+		}
+	};
 
-void goDown(matrix& inputMatrix, queue<Pipe>& BFS_Queue, Pipe nowPipe, int mapSize)
-{
-	if (nowPipe.pipeRear.first + 1 < mapSize && 
-		inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second] == 0)
+public:
+	Solution(int matrixSize, matrix inputMatrix)
 	{
-		nowPipe.pipeFront = nowPipe.pipeRear;
-		++nowPipe.pipeRear.first;
-		BFS_Queue.push(nowPipe);
+		this->matrixSize = matrixSize;
+		this->inputMatrix = inputMatrix;
 	}
-}
+	~Solution()
+	{
+	}
 
-void goDiagonal(matrix& inputMatrix, queue<Pipe>& BFS_Queue, Pipe nowPipe, int mapSize)
-{
-	if (nowPipe.pipeRear.second + 1 < mapSize && nowPipe.pipeRear.first + 1 < mapSize && 
-		inputMatrix[nowPipe.pipeRear.first][nowPipe.pipeRear.second + 1] == 0 &&
-		inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second] == 0 &&
-		inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second + 1] == 0)
+	int solution()
 	{
-		nowPipe.pipeFront = nowPipe.pipeRear;
-		++nowPipe.pipeRear.first;
-		++nowPipe.pipeRear.second;
-		BFS_Queue.push(nowPipe);
+		init();
+		BFS_Queue.push(Pipe());
+		int count = 0;
+
+		//Solution
+		while (!BFS_Queue.empty())
+		{
+			Pipe nowPipe = BFS_Queue.front();
+			BFS_Queue.pop();
+
+			if (nowPipe.pipeRear.first == matrixSize - 1 && nowPipe.pipeRear.second == matrixSize - 1)
+			{
+				++count;
+				continue;
+			}
+
+			if (nowPipe.pipeFront.first == nowPipe.pipeRear.first)
+			{
+				goRight(nowPipe);
+				goDiagonal(nowPipe);
+			}
+			else if (nowPipe.pipeFront.second == nowPipe.pipeRear.second)
+			{
+				goDiagonal(nowPipe);
+				goDown(nowPipe);
+			}
+			else
+			{
+				goRight(nowPipe);
+				goDiagonal(nowPipe);
+				goDown(nowPipe);
+			}
+		}
+
+		return count;
 	}
-}
+
+private:
+	int matrixSize;
+	matrix inputMatrix;
+	queue<Pipe> BFS_Queue;
+
+	void init()
+	{
+		BFS_Queue = queue<Pipe>();
+	}
+
+	void goRight(Pipe nowPipe)
+	{
+		if (nowPipe.pipeRear.second + 1 < matrixSize &&
+			inputMatrix[nowPipe.pipeRear.first][nowPipe.pipeRear.second + 1] == 0)
+		{
+			nowPipe.pipeFront = nowPipe.pipeRear;
+			++nowPipe.pipeRear.second;
+			BFS_Queue.push(nowPipe);
+		}
+	}
+
+	void goDown(Pipe nowPipe)
+	{
+		if (nowPipe.pipeRear.first + 1 < matrixSize &&
+			inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second] == 0)
+		{
+			nowPipe.pipeFront = nowPipe.pipeRear;
+			++nowPipe.pipeRear.first;
+			BFS_Queue.push(nowPipe);
+		}
+	}
+
+	void goDiagonal(Pipe nowPipe)
+	{
+		if (nowPipe.pipeRear.second + 1 < matrixSize && nowPipe.pipeRear.first + 1 < matrixSize &&
+			inputMatrix[nowPipe.pipeRear.first][nowPipe.pipeRear.second + 1] == 0 &&
+			inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second] == 0 &&
+			inputMatrix[nowPipe.pipeRear.first + 1][nowPipe.pipeRear.second + 1] == 0)
+		{
+			nowPipe.pipeFront = nowPipe.pipeRear;
+			++nowPipe.pipeRear.first;
+			++nowPipe.pipeRear.second;
+			BFS_Queue.push(nowPipe);
+		}
+	}
+};
 
 int main()
 {
@@ -85,42 +137,10 @@ int main()
 		}
 	}
 
-	queue<Pipe> BFS_Queue;
-	BFS_Queue.push(Pipe());
-	int count = 0;
-
-	//Solution
-	while (!BFS_Queue.empty())
-	{
-		Pipe nowPipe = BFS_Queue.front();
-		BFS_Queue.pop();
-
-		if (nowPipe.pipeRear.first == matrixSize - 1 && nowPipe.pipeRear.second == matrixSize - 1)
-		{
-			++count;
-			continue;
-		}
-
-		if (nowPipe.pipeFront.first == nowPipe.pipeRear.first)
-		{
-			goRight(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-			goDiagonal(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-		}
-		else if (nowPipe.pipeFront.second == nowPipe.pipeRear.second)
-		{
-			goDiagonal(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-			goDown(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-		}
-		else
-		{
-			goRight(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-			goDiagonal(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-			goDown(inputMatrix, BFS_Queue, nowPipe, matrixSize);
-		}
-	}
+	Solution sol(matrixSize, inputMatrix);
 
 	//Output
-	cout << count << '\n';
+	cout << sol.solution() << '\n';
 
 	////////////////////////////////////
 	system("pause");
