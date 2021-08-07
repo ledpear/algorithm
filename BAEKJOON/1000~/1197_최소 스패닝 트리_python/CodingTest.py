3ed80bdc41dcc038764f25ceb03867cd62df419b
ledpear <tjsrb75@gmail.com>
2021-08-07 11:51:15 +0900

    1197 최소 스패닝 트리 파이썬 통과 : prim 알고리즘

---
+++ BAEKJOON/1000~/1197_최소 스패닝 트리_python/CodingTest.py
@@ -0,0 +1,36 @@
+import string
+import sys
+import heapq
+
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+#n, m = map(int, input().split())
+#k = input().rstrip()
+
+nodeSize, edgeSize = map(int, input().split())
+edges = [[] for _ in range(nodeSize + 1)]
+for _ in range(edgeSize):
+    nodeA, nodeB, cost = map(int, input().split())
+    edges[nodeA].append([nodeB, cost])
+    edges[nodeB].append([nodeA, cost])
+
+visit = [False] * (nodeSize + 1)
+hq = [[0, 1]] #cost, node
+visitCount = 0
+answer = 0
+
+while visitCount != nodeSize:
+    nowCost, nowNode = heapq.heappop(hq)
+    if visit[nowNode]:
+        continue
+
+    visit[nowNode] = True
+    visitCount += 1
+    answer += nowCost
+
+    for targetNode, targetCost in edges[nowNode]:
+        if not visit[targetNode]:
+            heapq.heappush(hq, [targetCost, targetNode])
+
+print(answer)
