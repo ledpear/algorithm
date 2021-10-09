d85c63aa0ecc76997f359beffe325083c83e142f
ledpear <tjsrb75@gmail.com>
2021-10-10 01:02:45 +0900

    unique_ptr 만들기 : unique_ptr[] 구현 완료

--- Test/unique_ptr/unique_ptr/Source.cpp
+++ Test/unique_ptr/unique_ptr/Source.cpp
@@ -1,5 +1,6 @@
 #include "source.h"
 #include <algorithm>
+#include <iostream>
 
 namespace sg
 {
@@ -36,7 +37,6 @@ namespace sg
 		}		
 	}
 
-	//Observers
 	template<class T>
 	void Ptr<T>::swap(Ptr& rhs)
 	{
@@ -48,53 +48,66 @@ namespace sg
 	//PtrArr
 
 	//Move assignment operator
-// 	template<class T>
-// 	PtrArr<T>& PtrArr<T>::operator= (PtrArr&& rhs)
-// 	{
-// 		reset(rhs.release());
-// 		return (*this);
-// 	}
-// 
-// 	//Modifiers
-// 	template<class T>
-// 	T* PtrArr<T>::release()
-// 	{
-// 		T* oldPtr = get();
-// 		_ptr = PtrArr();
-// 		return oldPtr;
-// 	}
-// 
-// 	template<class T>
-// 	void PtrArr<T>::reset(T* newPtr = nullptr)
-// 	{
-// 		T* oldPtr = get();
-// 		_ptr = newPtr;
-// 		if (oldPtr != _ptr)
-// 		{
-// 			if (oldPtr != nullptr)
-// 			{
-// 				delete oldPtr;
-// 			}
-// 		}
-// 	}
-// 
-// 	//Observers
-// 	template<class T>
-// 	void PtrArr<T>::swap(Ptr& rhs)
-// 	{
-// 		std::swap(_ptr, rhs._ptr);
-// 	}
+	template<class T>
+	PtrArr<T>& PtrArr<T>::operator= (PtrArr&& rhs)
+	{
+		reset(rhs.release());
+		return (*this);
+	}
+
+ 	//Modifiers
+ 	template<class T>
+ 	T* PtrArr<T>::release()
+ 	{
+ 		T* oldPtr = get();
+ 		_ptr = PtrArr();
+ 		return oldPtr;
+ 	}
+ 
+	template<class T>
+	void PtrArr<T>::reset(T* newPtr = nullptr)
+	{
+		T* oldPtr = get();
+		_ptr = newPtr;
+		if (oldPtr != _ptr)
+		{
+			if (nullptr != oldPtr)
+			{
+				delete[] oldPtr;
+			}
+		}
+	}
+
+ 	template<class T>
+ 	void PtrArr<T>::swap(PtrArr& rhs)
+ 	{
+ 		std::swap(_ptr, rhs._ptr);
+ 	}
 }
 
 int main()
 {
-	int a = 5;
-	int b = 8;
+	int *a = new int(5);
+	int *b = new int(8);
 
- 	sg::Ptr<int> pa(&a);
- 	sg::Ptr<int> pb(&b);
+	sg::Ptr<int> pa(a);
+	sg::Ptr<int> pb(b);
 
 	pa.swap(pb);
 
-	return 0;
+
+	/////////////////////////////////////////////////////////////////////////////////
+	//PtrArr
+
+	int *arrA = new int[3]{ 1, 2, 3 };
+	int *arrB = new int[3]{ 4, 5, 6 };
+
+	sg::PtrArr<int> paArr(arrA);
+	sg::PtrArr<int> pbArr(arrB);
+
+	paArr.swap(pbArr);
+
+	for (int i = 0; i < 3; ++i)
+		std::cout << paArr[i] << '\n';
+
 }
--- Test/unique_ptr/unique_ptr/source.h
+++ Test/unique_ptr/unique_ptr/source.h
@@ -29,43 +29,43 @@ namespace sg
 		//Observers
 		T* get() const { return _ptr; }
 		//get_deleter();
-		explicit operator bool() const { return (get() != Ptr()); }
+		explicit operator bool() const { return (get() != nullptr); }
 
 		//Single-object version
 		T& operator*() const { return *_ptr };
 		T* operator->() const { return _ptr };
 	};
 
-//  	template <typename T>
-//  	class PtrArr
-//  	{
-//  	private:
-//  		T* _ptr;
-// 		void deleter() { if (_ptr != nullptr) delete[] _ptr; }
-//  	public:
-// 		explicit Ptr(T* readPtr = nullptr) : _ptr(readPtr) {}
-// 		~Ptr() { deleter(); }
-// 
-// 		//Copy constructor
-// 		Ptr(const Ptr& rhs) = delete;
-// 		//Move constructor
-// 		Ptr(Ptr&& rhs) : _ptr(rhs->release()) {}
-// 		//Assignment operator
-// 		Ptr& operator= (const Ptr& rhs) = delete;
-// 		//Move assignment operator
-// 		Ptr& operator= (Ptr&& rhs);
-// 
-// 		//Modifiers
-// 		T* release();
-// 		void reset(T* newPtr = nullptr);
-// 		void swap(Ptr& rhs);
-// 
-// 		//Observers
-// 		T* get() const { return _ptr; }
-// 		//get_deleter();
-// 		explicit operator bool() const { return (get() != Ptr()); }
-//  
-//  		//Array version
-//  		T& operator[](int index);
-//  	};
+  	template <typename T>
+  	class PtrArr
+  	{
+  	private:
+  		T* _ptr;
+ 		void deleter() { if (_ptr != nullptr) delete[] _ptr; }
+  	public:
+ 		explicit PtrArr(T* readPtr = nullptr) : _ptr(readPtr) {}
+ 		~PtrArr() { deleter(); }
+ 
+ 		//Copy constructor
+ 		PtrArr(const PtrArr& rhs) = delete;
+ 		//Move constructor
+ 		PtrArr(PtrArr&& rhs) : PtrArr(rhs->release()) {}
+ 		//Assignment operator
+ 		PtrArr& operator= (const PtrArr& rhs) = delete;
+ 		//Move assignment operator
+ 		PtrArr& operator= (PtrArr&& rhs);
+ 
+ 		//Modifiers
+ 		T* release();
+ 		void reset(T* newPtr = nullptr);
+ 		void swap(PtrArr& rhs);
+ 
+ 		//Observers
+ 		T* get() const { return _ptr; }
+ 		//get_deleter();
+ 		explicit operator bool() const { return (get() != nullptr); }
+  
+  		//Array version
+		T& operator[](size_t idx) const { return (get()[idx]); }
+  	};
 }
