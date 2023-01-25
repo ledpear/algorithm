5e11faff35343425d52379a5d4732733508ba89d
LEDPEAR <tjsrb75@gmail.com>
2023-01-25 23:58:50 +0900

    레벨2 숫자 블록 효율성 통과

--- PROGRAMMERS/Level 2/숫자 블록/Source.cpp
+++ PROGRAMMERS/Level 2/숫자 블록/Source.cpp
@@ -13,60 +13,37 @@ vector<int> solution(long long begin, long long end)
 	vector<int> answer;
 	//최대 약수를 구하기 위해 소수부터 구한다.
 	vector<int> decimal;
+	const int maxVal = static_cast<int>(sqrtf(end));
+	for (int i = begin; i <= end; ++i)
 	{
-		int max = static_cast<int>( sqrtf(end) ) + 1;
-		vector<bool> check = vector<bool>(max);
-		for (int i = 2; i < max; ++i)
+		if (decimal.empty())
 		{
-			if (check[i] == false)
-			{
-				decimal.push_back(i);
+			answer.push_back(0);
+			continue;
+		}
 
-				int j = 2;
-				while (i * j < max)
-				{
-					check[i * j] = true;
-					++j;
-				}
+		if (i < decimal[0])
+		{
+			answer.push_back(0);
+			continue;
+		}
+
+		int num = 1;
+		for (int j = 2; j <= maxVal; ++j)
+		{
+			if (i < j)
+				break;
+
+			if (i % j == 0)
+			{
+				int result = i / j;
+				if (result <= 10000000 && j <= 10000000)
+					num = max(num, result);
 			}
 		}
-	}
 
-	{
-		for (int i = begin; i <= end; ++i)
-		{
-			if (decimal.empty())
-			{
-				answer.push_back(0);
-				continue;
-			}
-
-			if( i < decimal[0])
-			{
-				answer.push_back(0);
-				continue;
-			}
-
-			int num = 1;
-			for (int j = 0; j < decimal.size(); ++j)
-			{
-				if (i < decimal[j])
-					break;
-
-				if (i % decimal[j] == 0)
-				{
-					num = i / decimal[j];
-					if (num > 10000000)
-						num = decimal[j];
-					else
-						break;
-				}
-			}
-
-			answer.push_back(num);
-		}
-	}
-	
+		answer.push_back(num);
+	}	
 
 	return answer;
 }
@@ -75,7 +52,9 @@ int main()
 {
 	vector<int> test;
 	{
-		test = solution(999990000, 1000000000);
+		long begin = 1000000000 - 9;
+		long end = 1000000000;
+		test = solution(begin, end);
 	}
 	test = solution(1, 25);
 
