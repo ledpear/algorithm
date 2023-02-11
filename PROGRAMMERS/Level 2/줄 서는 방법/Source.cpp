8f9f7dda72467e658b38e56b0941e83017961730
LEDPEAR <tjsrb75@gmail.com>
2023-02-11 21:24:56 +0900

    레벨2 줄 서는 방법 통과

--- PROGRAMMERS/Level 2/줄 서는 방법/Source.cpp
+++ PROGRAMMERS/Level 2/줄 서는 방법/Source.cpp
@@ -4,89 +4,101 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
+#include <cassert>
 
 using namespace std;
 
-vector<int> solution(int n, long long k) 
+vector<long long> getFactorialNumber(int size)
 {
-	vector<int> answer;
-	
+	//index는 구하려는 팩토리얼
 	vector<long long> result = vector<long long>{ 1, 1 };
-	//일단 n에 따른 가짓수를 구한다.
-	for (int i = 2; i <= n; ++i)
-	{
+	for (int i = 2; i <= size; ++i)
 		result.push_back(i * result[i - 1]);
-	}
 
-	//그 다음은 몫으로 순서를 찾는다
-	//1. 몫은 순서
-	//2. 나머지는 다음 차례의 n
-	int count = n;
-	int div = n;
-	vector<bool> used = vector<bool>(n + 1);
-	int value = k;
-	while (count > 2)
+	return result;
+}
+
+int searchNumber(int targetCount, vector<bool>& log)
+{
+	int searchCount = 0;
+	int index = 1;
+	for (; index < log.size(); ++index)
 	{
-		--count;
-		//처음에는 K 그 다음은 몫
-		//몫 나누기 [이전 가짓수] 하면 
-		int targetCount = (value - 1) / result[count];
-		for (int index = 1; index <= n; ++index)
-		{
-			if (used[index] == true)
-				continue;
-
-			if (targetCount == 0)
-			{
-				answer.push_back(index);
-				used[index] = true;
-				break;
-			}
-
-			--targetCount;
-		}
-
-		value = value % result[count];
-	}
-
-	int num1 = 0;
-	int num2 = 0;
-	for (int index = 1; index <= n; ++index)
-	{
-		if (used[index] == true)
+		if (log[index] == true)
 			continue;
 
-		if (num1 == 0)
-			num1 = index;
-		else
+		++searchCount;
+
+		if (searchCount == targetCount)
 		{
-			num2 = index;
+			log[index] = true;
 			break;
 		}
 	}
 
-	if (value == 1)
+	return index;
+}
+
+vector<int> solution(int n, long long k)
+{
+	vector<int> answer;
+
+	//일단 n에 따른 가짓수를 구한다.
+	vector<long long> factorialNumber = getFactorialNumber(n);
+
+	// 구할 숫자는
+	// ((k / (n-1)의 팩토리얼) + 1) 번째 숫자
+	// 구한다음에 k는 
+	// k % (n-1)의 팩토리얼 인데 만약 0이라면  (n-1)의 팩토리얼이 된다
+
+	vector<bool> log(n + 1);
+	int currNum = n;
+	while (answer.size() != n)
 	{
-		answer.push_back(num1);
-		answer.push_back(num2);
-	}
-	else
-	{
-		answer.push_back(num2);
-		answer.push_back(num1);
+		//몇번째 숫자를 찾을건지 구한다
+		const long long currFactorialNum = factorialNumber[currNum - 1];
+		if (currFactorialNum == 1)
+		{
+			answer.push_back(searchNumber(k, log));
+			answer.push_back(searchNumber(1, log));
+			break;
+		}
+
+		assert(k > 0);
+		const long long target = ((k - 1) / currFactorialNum) + 1;
+		answer.push_back(searchNumber(target, log));
+		k %= factorialNumber[currNum - 1];
+		if (k == 0)
+			k = factorialNumber[currNum - 1];
+
+		--currNum;
 	}
 
 	return answer;
 }
 
+void print(vector<int> arr)
+{
+	for (int num : arr)
+		cout << num << ' ';
+	cout << endl;
+}
+
+void solPrint(int n)
+{
+	vector<long long> size = getFactorialNumber(n);
+	for (int i = 1; i <= size[n]; ++i)
+	{
+		print(solution(n, i));
+	}
+}
+
 int main()
 {
-	vector<int> answer;
-	{
-		answer = solution(3, 5);
-	}
-	{
-		answer = solution(4, 4);
-	}
+	for (int i = 1; i <= 7; ++i)
+		solPrint(i);
+
+	vector<int> answer = solution(3, 6);
+
 	return 0;
 }
