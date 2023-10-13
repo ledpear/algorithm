5420c60f43b38a3a752ac0c6f5c4d24124005ac6
LEDPEAR <tjsrb75@gmail.com>
2023-10-13 15:28:19 +0900

    C# 샘플 코드 추가 및 ignore 업데이트

---
+++ _CodingTest_C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingTest
+{
+    public class Solution
+    {
+    }
+
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            //var result = sol.solution(2, 3);
+            //Console.WriteLine(result);
+        }
+    }
+}
