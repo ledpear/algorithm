4866ec4f62fd66ec04c0fa56ae4e55a40d9f9daa
LEDPEAR <tjsrb75@gmail.com>
2023-11-04 22:30:27 +0900

    1806 부분합 C++ 통과 : 투포인터

---
+++ BAEKJOON/1000~/1806_부분합/_CodingTest_C++.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int INF = 100000000;
+
+int main()
+{
+	int n, s;
+	cin >> n >> s;
+	vector<int> numbers(n, 0);
+	for (int i = 0; i < n; ++i)
+		cin >> numbers[i];
+
+	int minLength = INF;
+	int numberSum = 0;
+	int startIndex = 0;
+	int endIndex = 0;
+	for (int endIndex = 0; endIndex < n; ++endIndex)
+	{
+		numberSum += numbers[endIndex];
+		while (numberSum >= s)
+		{
+			if (startIndex > endIndex)
+				break;
+
+			minLength = min(minLength, endIndex - startIndex + 1);
+			numberSum -= numbers[startIndex];
+			++startIndex;
+		}
+	}
+
+	if (minLength == INF)
+		minLength = 0;
+
+	cout << minLength << endl;
+}
