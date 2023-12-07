f0a4b4de40a1e180f6eeaee7bf494327b4d0c639
LEDPEAR <tjsrb75@gmail.com>
2023-12-07 22:12:09 +0900

    2512 예산 C++ 통과 : 이분탐색

---
+++ BAEKJOON/2000~/2512_예산_C++/_CodingTest_C++.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int size = 0;
+    cin >> size;
+    vector<int> arr(size, 0);
+    for (int& num : arr)
+        cin >> num;
+
+    int maxVal = 0;
+    cin >> maxVal;
+
+    int left = 0, right = *max_element(arr.begin(), arr.end());
+    int answer = 0;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        int sum = 0;
+        for (const int num : arr)
+            sum += mid < num ? mid : num;
+
+        if (sum <= maxVal)
+        {
+            answer = max(answer, mid);
+            left = mid + 1;
+        }
+        else
+            right = mid - 1;
+    }
+
+    cout << answer << endl;
+}
