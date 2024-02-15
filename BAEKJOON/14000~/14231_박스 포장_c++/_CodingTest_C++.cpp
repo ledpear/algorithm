d747c9f9c2d12a8f423f8a67c67b8a49597e4561
LEDPEAR <tjsrb75@gmail.com>
2024-02-15 21:55:57 +0900

    14231 박스 포장 C++ 통과 : DP

---
+++ BAEKJOON/14000~/14231_박스 포장_c++/_CodingTest_C++.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int size = 0;
+	cin >> size;
+	vector<int> boxs(size, 0);
+	for (int& box : boxs)
+		cin >> box;
+
+	//sol
+	vector<int> dpArr(size, 1);
+	int result = 0;
+	for (int current = 1; current < size; ++current)
+	{
+		const int currentNum = boxs[current];
+		int maxCount = 0;
+		//dpArr를 뒤로 순회해서 자신보다 작으면서 가장 큰 카운트를 찾는다.
+		for (int target = current - 1; target >= 0; --target)
+		{
+			const int targetNum = boxs[target];
+			if (currentNum > targetNum)
+				maxCount = max(maxCount, dpArr[target]);
+		}
+
+		dpArr[current] += maxCount;
+	}
+
+	cout << *max_element(dpArr.begin(), dpArr.end());
+}
