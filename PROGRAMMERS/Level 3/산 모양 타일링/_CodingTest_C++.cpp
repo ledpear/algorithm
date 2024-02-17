54c3e4f993046ae3d98dad27dfb887dcde22a306
LEDPEAR <tjsrb75@gmail.com>
2024-02-17 15:40:19 +0900

    레벨3 산 모양 타일링 C++ 통과 : DP

--- PROGRAMMERS/Level 3/산 모양 타일링/_CodingTest_C++.cpp
+++ PROGRAMMERS/Level 3/산 모양 타일링/_CodingTest_C++.cpp
@@ -13,6 +13,7 @@ int solution(int n, vector<int> tops)
 {
     vector<int> dpArr(n + 1, 0);
     vector<int> dpArr2(n + 1, 0);
+    dpArr[0] = 1;
     if (tops[0] == 1)
     {
         dpArr[1] = 4;
@@ -27,6 +28,7 @@ int solution(int n, vector<int> tops)
     for (int i = 2; i <= n; ++i)
     {
         unsigned long long val = 0;
+        unsigned long long val2 = 0;
         if (tops[i - 1] == 1)
             val = dpArr[i - 1] * 3;
         else
@@ -34,12 +36,14 @@ int solution(int n, vector<int> tops)
 
         val += dpArr2[i - 1];
 
-        if (tops[i - 2] == 1)
-            val += dpArr2[i - 1];
+        //dpArr2 업데이트
+        if (tops[i - 1] == 1)
+            val2 = (dpArr2[i - 1] * 3) + (dpArr[i - 2] * 2);
         else
-            val += 2;
+            val2 = dpArr2[i - 1] * 2 + dpArr[i - 2];
 
         dpArr[i] = val % 10007;
+        dpArr2[i] = val2 % 10007;
     }
 
     return dpArr[n];
@@ -47,7 +51,7 @@ int solution(int n, vector<int> tops)
 
 int main()
 {
-    //cout << solution(4, { 1,1, 0, 1 }) << endl;
-    cout << solution(2, { 1, 1 }) << endl;
-    //cout << solution(10, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }) << endl;
+    cout << solution(4, { 1,1, 0, 1 }) << endl;
+    cout << solution(2, { 0, 1 }) << endl;
+    cout << solution(10, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }) << endl;
 }
