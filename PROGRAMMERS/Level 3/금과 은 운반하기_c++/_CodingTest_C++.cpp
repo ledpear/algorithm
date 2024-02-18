e186ac5b92789695817f87c55d2c36205c78ed49
LEDPEAR <tjsrb75@gmail.com>
2024-02-18 13:34:30 +0900

    레벨3 금과 은 운반하기 중간 저장

---
+++ PROGRAMMERS/Level 3/금과 은 운반하기_c++/_CodingTest_C++.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <queue>
+
+using namespace std;
+
+struct Info
+{
+    int gold;
+    int silver;
+    int weight;
+    int time;
+    int nextTime = 0;
+
+    Info(int g, int s, int w, int t) 
+        : gold(g), silver(s), weight(w), time(t) 
+    {
+        nextTime = time;
+    }
+};
+
+struct comp
+{
+    bool operator()(const Info& lhs, const Info& rhs)
+    {
+        return lhs.nextTime > rhs.nextTime;
+    }
+};
+
+//트럭은 각 마을마다 있고 동시 운용이 가능하다
+//a는 필요한 금, b는 필요한 은
+//g는 각 마을별 금, s는 각 마을별 은
+//w는 각 마을별 트럭의 운용가능 무게, t는 시간
+
+//금을 다 모을수 있는 최소시간
+//은을 다 모을수 있는 최소시간
+//각각 구한 후 더 큰쪽으로 했을때 반대쪽이 넘지 않으면 그게 최소가 될듯
+
+//금을 우선시 했을때 최소시간
+//은을 우선시 했을때 최소시간
+//중 더 적은 시간
+
+//우선순위 큐를 사용해서 정렬한다
+
+
+int simulation(priority_queue<Info, vector<Info>, comp> pq, int needGold, int needSilver, bool isGoldFirst)
+{
+    int currTime = 0;
+    int currFirst = 0, currSecond = 0;
+    int needFirst = isGoldFirst ? needGold : needSilver;
+    int needSecond = isGoldFirst ? needSilver : needGold;
+
+    while (currFirst < needFirst || currSecond < needSecond)
+    {
+        //다음차례
+        Info truck = pq.top();
+        pq.pop();
+
+        //시간 경과
+        if (currTime < truck.nextTime)
+            currTime = truck.nextTime;
+
+        //우선순위 적용
+        int& firstMetal = isGoldFirst ? truck.gold : truck.silver;
+        int& secondMetal = isGoldFirst ? truck.silver : truck.gold;
+
+        //gold 이동
+        int moveFirst = firstMetal > truck.weight ? truck.weight : firstMetal;
+        if (needFirst - currFirst < moveFirst)
+            moveFirst = needFirst - currFirst;
+
+        firstMetal -= moveFirst;
+        currFirst += moveFirst;
+
+        //silver 이동
+        if (moveFirst != truck.weight)
+        {
+            int posibleWdight = truck.weight - moveFirst;
+            int moveSecond = secondMetal > posibleWdight ? posibleWdight : secondMetal;
+            secondMetal -= moveSecond;
+            currSecond += moveSecond;
+        }
+
+        //시간 갱신 및 큐에 추가
+        if (truck.gold != 0 || truck.silver != 0)
+        {
+            truck.nextTime += truck.time * 2;
+            pq.push(truck);
+        }
+    }
+
+    return currTime;
+}
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) 
+{
+    long long answer = -1;
+
+    priority_queue<Info, vector<Info>, comp> ori_pq;
+    for (int i = 0; i < g.size(); ++i)
+        ori_pq.push(Info(g[i], s[i], w[i], t[i]));
+
+    int minGold = simulation(ori_pq, a, b, true);
+    int minSilver = simulation(ori_pq, a, b, false);
+    int minTime = minGold < minSilver ? minGold : minSilver ;
+
+    return minTime;
+}
+
+int main()
+{
+    //solution(10, 10, { 100 }, { 100 }, { 7 }, { 10 });
+    //solution(90, 500, { 70,70,0 }, { 0,0,500 }, { 100,100,2 }, { 4,8,1 });
+    solution(100, 100, { 70,10,100 }, { 10,40,100 }, {25,10,5 }, { 2,3,5 });
+}
---
+++ PROGRAMMERS/Level 3/금과 은 운반하기_v.2_c++/_CodingTest_C++.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <queue>
+
+using namespace std;
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) 
+{
+    long long answer = -1;
+
+    long long minVal = 0, maxVal = 1e5 + (1e5 * 2) * (2 * 1e9);
+
+    //이분탐색
+    //최소와 최저의 중간으로 최적해를 찾는다
+    //시간을 대입해서 g와 s가 만족하는지 확인하고 만족한다면 맥스를 만족하지 못한다면 민은 바꾼다.
+    while (minVal <= maxVal)
+    {
+        //안되면 더해서 나누는 걸로 바꾸자
+        long long currTime = (maxVal / 2) + (minVal / 2);
+        long long gold = 0;
+        long long silver = 0;
+        long long weight = 0;
+        for (int i = 0; i < g.size(); ++i)
+        {
+            long long count = (currTime + t[i]) / (t[i] * 2);
+            gold += min(static_cast<long long>(g[i]), static_cast<long long>(count * w[i]));
+            silver += min(static_cast<long long>(s[i]), static_cast<long long>(count * w[i]));
+            weight += min(static_cast<long long>(g[i]+s[i]), static_cast<long long>(count * w[i]));
+        }
+
+        if (gold >= a && silver >= b && weight >= a + b)
+        {
+            answer = currTime;
+            maxVal = currTime - 1;
+        }
+        else
+            minVal = currTime + 1;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(10, 10, { 100 }, { 100 }, { 7 }, { 10 });
+    solution(90, 500, { 70,70,0 }, { 0,0,500 }, { 100,100,2 }, { 4,8,1 });
+    solution(100, 100, { 70,10,100 }, { 10,40,100 }, {25,10,5 }, { 2,3,5 });
+}
